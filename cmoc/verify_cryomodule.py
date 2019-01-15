00d4fd8dcd1615090cd4bee23bb5ee248bacf458
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2019-01-14 16:28:43 -0800

    verify_cryomodule.py: Restore cryomodule_check

--- cmoc/verify_cryomodule.py
+++ cmoc/verify_cryomodule.py
@@ -5,22 +5,19 @@ import sys
 from matplotlib import pyplot as plt
 
 
-def read(mag=True):
+def read():
     A = np.loadtxt('cryomodule_p.dat')
     cav = A[:, 0] + 1j*A[:, 1]
     fwd = A[:, 2] + 1j*A[:, 3]
     rfl = A[:, 4] + 1j*A[:, 5]
-    if mag:
-        return abs(cav), abs(fwd), abs(rfl)
-    else:
-        return ang(cav), ang(fwd), ang(rfl)
+    return cav, fwd, rfl
 
 
-def show(data):
+def show(data, f_cvt=abs):
     cav, fwd, rfl = data
-    plt.plot(cav, label='cav')
-    plt.plot(fwd, label='fwd')
-    plt.plot(rfl, label='rfl')
+    plt.plot(f_cvt(cav), label='cav')
+    plt.plot(f_cvt(fwd), label='fwd')
+    plt.plot(f_cvt(rfl), label='rfl')
     plt.legend()
     plt.show()
 
@@ -42,11 +39,10 @@ def check_err(test_val, bound, err):
 def make_check(data):
     cav, fwd, rfl = data
     l = len(cav) // 2
-    err_bar = 1
-    print(cav[l], fwd[l], rfl[l])
-    fail_pass(check_err(cav[l], 11, err_bar) and
-              check_err(fwd[l], 20, err_bar) and
-              check_err(rfl[l], 17, err_bar))
+    err_bar = 5
+    fail_pass(check_err(abs(cav[l]), 1934, err_bar) and
+              check_err(abs(fwd[l]), 1318, err_bar) and
+              check_err(abs(rfl[l]), 847, err_bar))
 
 
 if __name__ == "__main__":
@@ -56,10 +52,12 @@ if __name__ == "__main__":
                         help='Purely run the check')
     parser.add_argument('-s', '--show', action='store_true',
                         help='Show plots first and then run the check')
+    parser.add_argument('-p', '--phase', action='store_true',
+                        help='Choose phase instead of default magnitude plot')
     args = parser.parse_args()
-    data = read(mag=True)
+    data = read()
     if args.show:
-        show(data)
+        show(data, f_cvt=ang if args.phase else abs)
         make_check(data)
     else:
         make_check(data)
