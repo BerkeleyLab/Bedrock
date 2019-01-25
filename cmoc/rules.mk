058e747d5ff2d132bf3eaffc4376e74ee172ceb0
Vamsi K Vytla <vkvytla@lbl.gov>
2019-01-25 13:23:52 -0800

    cryomodule_badger_tb wasn't getting made or deleted

--- cmoc/rules.mk
+++ cmoc/rules.mk
@@ -1,10 +1,10 @@
-TEST_BENCH = xy_pi_clip_tb fdbk_core_tb tgen_tb circle_buf_tb cryomodule_tb
+TEST_BENCH = xy_pi_clip_tb fdbk_core_tb tgen_tb circle_buf_tb cryomodule_tb cryomodule_badger_tb
 
 include $(CMOC_DIR)/api.mk
 
 TGT_ := $(TEST_BENCH)
 
-NO_CHECK = tgen_check
+NO_CHECK = tgen_check cryomodule_badger_check
 CHK_ = $(filter-out $(NO_CHECK), $(TEST_BENCH:%_tb=%_check))
 
 .PHONY: targets checks bits check_all clean_all
