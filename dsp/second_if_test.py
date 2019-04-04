54ece79e63eefad92d43591e05bf2e55adf93637
Sergio Paiagua <smferreirapaiagua@lbl.gov>
2019-04-04 14:26:42 -0700

    PEP8 fixes

--- dsp/second_if_test.py
+++ dsp/second_if_test.py
@@ -3,7 +3,7 @@ import sys
 
 fname = sys.argv[1]
 with open(fname, 'r') as f:
-	a = [int(x) for x in f.readlines()]
+    a = [int(x) for x in f.readlines()]
 
 npt = 132*2
 a = a[300:300+npt]
@@ -11,9 +11,9 @@ ss = numpy.abs(numpy.fft.fft(a))
 peak = numpy.max(ss)
 fail = 0
 for ix in range(int(npt/2)):
-	if ss[ix] > 0.003*peak:  # require spurs less than -50 dBc
-		print("%d %f" % (ix, ss[ix]))
-		if ix != 61:
-			fail = 1
+    if ss[ix] > 0.003*peak:  # require spurs less than -50 dBc
+        print("%d %f" % (ix, ss[ix]))
+        if ix != 61:
+            fail = 1
 print("PASS" if not fail else "FAIL")
 sys.exit(fail)
