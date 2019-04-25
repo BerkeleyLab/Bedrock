c453bc82a7c6b2295833c43a00cbb9b45aeb1c8d
Sergio Paiagua <smferreirapaiagua@lbl.gov>
2019-04-24 18:16:33 -0700

    Updating comms_top/Makefile to work with latest version of Bedrock. Adding comms_top.bit generation to CI

--- build-tools/vivado_tcl/project_proc.tcl
+++ build-tools/vivado_tcl/project_proc.tcl
@@ -114,6 +114,7 @@ proc project_run_planahead {project_name} {
     # Launch Synthesis
     launch_runs synth_1
     wait_on_run synth_1
+    open_run synth_1
 
     # Launch Implementation
     # planAhead: NGDBuild, MAP, PAR, TRCE, XDL, Bitgen
@@ -141,7 +142,6 @@ proc project_run {verilog_defines} {
 
     launch_runs synth_1
     wait_on_run synth_1
-    open_run synth_1
 
     #launch_runs impl_1 -to_step write_bitstream
     launch_runs impl_1 -to_step route_design
