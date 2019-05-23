cd0b2a87860797cd843f00c400b142da474d2b4e
Eric Norum <wenorum@lbl.gov>
2019-05-23 08:21:27 -0700

    Add 'small' version to go along with existing 'tiny' version.  Provides coverage of a lot of applications needing simple event receiver support.

--- dsp/tinyEVR_tb.v
+++ dsp/tinyEVR_tb.v
@@ -25,37 +25,69 @@
 module tinyEVR_tb;
 
 parameter TIMESTAMP_WIDTH = 64;
+parameter ACTION_WIDTH    = 4;
+parameter EVSTROBE_COUNT  = 126;
+
 parameter EVCODE_SHIFT_ZERO     = 8'h70;
 parameter EVCODE_SHIFT_ONE      = 8'h71;
 parameter EVCODE_SECONDS_MARKER = 8'h7D;
 
-reg          clk = 1;
+reg                    sysClk = 1;
+reg                    sysActionWriteEnable = 1'b0;
+reg              [7:0] sysActionAddress = {8{1'bx}};
+reg [ACTION_WIDTH-1:0] sysActionData = {ACTION_WIDTH{1'bx}};
+
+reg          evrClk = 1;
 reg   [15:0] evrRxWord = 16'hx;
 reg    [1:0] evrCharIsK = 2'hx;
 
 wire                       ppsMarker, timestampValid;
-wire [TIMESTAMP_WIDTH-1:0] timestamp;
-wire               [126:1] evStrobe;
+wire                       ppsMarker_s, timestampValid_s;
+wire [TIMESTAMP_WIDTH-1:0] timestamp, timestamp_s; 
+wire               [EVSTROBE_COUNT:1] evStrobe;
+wire    [ACTION_WIDTH-1:0] action;
 
-tinyEVR tinyEVR (.evrRxClk(clk),
-                 .evrRxWord(evrRxWord),
-                 .evrCharIsK(evrCharIsK),
-                 .ppsMarker(ppsMarker),
-                 .timestampValid(timestampValid),
-                 .timestamp(timestamp),
-                 .evStrobe(evStrobe));
+tinyEVR #(.EVSTROBE_COUNT(EVSTROBE_COUNT))
+  tinyEVR (.evrRxClk(evrClk),
+           .evrRxWord(evrRxWord),
+           .evrCharIsK(evrCharIsK),
+           .ppsMarker(ppsMarker),
+           .timestampValid(timestampValid),
+           .timestamp(timestamp),
+           .evStrobe(evStrobe));
+
+smallEVR #(.ACTION_WIDTH(ACTION_WIDTH))
+  smallEVR (.evrRxClk(evrClk),
+           .evrRxWord(evrRxWord),
+           .evrCharIsK(evrCharIsK),
+           .ppsMarker(ppsMarker_s),
+           .timestampValid(timestampValid_s),
+           .timestamp(timestamp_s),
+           .action(action),
+           .sysClk(sysClk),
+           .sysActionWriteEnable(sysActionWriteEnable),
+           .sysActionAddress(sysActionAddress),
+           .sysActionData(sysActionData));
 
 always begin
-    #4 clk <= !clk;
+    #5 sysClk <= !sysClk;
+end
+always begin
+    #4 evrClk <= !evrClk;
 end
 
-integer pass = 1;
-
+integer i;
 initial
 begin
-    $dumpfile("tinyEVR.lxt");
+    $dumpfile("tinyEVR_tb.lxt");
     $dumpvars(0, tinyEVR_tb);
 
+    #40 ;
+    for (i = 0 ; i < 256 ; i += 1) begin
+        setAction(i, 4'b0000);
+    end
+    setAction(EVCODE_SECONDS_MARKER, 4'b0010);
+    setAction(8'hBC, 4'b1111);
     #40 ;
     sendEvent(EVCODE_SECONDS_MARKER);
     check(32'h00000000);
@@ -74,10 +106,25 @@ begin
     #100 ;
     sendEvent(EVCODE_SECONDS_MARKER);
     check(32'h1234567A);
-    #1000;
-    $display("%s", pass ? "PASS" : "FAIL");
-    $finish;
+    #1000 $finish;
 end
+
+task setAction;
+    input              [7:0] evCode;
+    input [ACTION_WIDTH-1:0] evAction;
+    begin
+    @(posedge sysClk) begin
+        sysActionWriteEnable <= 1'b1;
+        sysActionAddress <= evCode;
+        sysActionData <= evAction;
+    end
+    @(posedge sysClk) begin
+        sysActionWriteEnable <= 1'b0;
+        sysActionAddress <= {8{1'bx}};
+        sysActionData <= {ACTION_WIDTH{1'bx}};
+    end
+    end
+endtask
 
 task sendSeconds;
     input [31:0] arg;
@@ -92,15 +139,23 @@ endtask
 task sendEvent;
     input [7:0] arg;
     begin
-    @(posedge clk) begin
+    @(posedge evrClk) begin
         evrRxWord[7:0] = arg;
         evrCharIsK = 2'bx0;
     end
-    @(posedge clk) begin
-        evrRxWord[7:0] = 8'bx;
-        evrCharIsK = 2'bx1;
+    @(posedge evrClk) begin
+        evrRxWord[7:0] = 8'h00;
+        evrCharIsK = 2'bx0;
     end
-    @(posedge clk) ;
+    @(posedge evrClk) begin
+        evrRxWord[7:0] = 8'hBC;
+        evrCharIsK = 2'bx01;
+    end
+    @(posedge evrClk) begin
+        evrRxWord[7:0] = 8'h00;
+        evrCharIsK = 2'bx0;
+    end
+    @(posedge evrClk) ;
     end
 endtask
 
@@ -110,7 +165,6 @@ task check;
     begin
     seconds = timestamp[32+:32];
     $display("%x %x %s", arg, seconds, (arg == seconds) ? "PASS" : "FAIL");
-    if (arg != seconds) pass = 0;
     end
 endtask
 
