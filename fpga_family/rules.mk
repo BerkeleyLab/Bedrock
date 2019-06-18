702fe1925d01b87e7eab470b1039461c6840cc35
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2019-06-18 14:21:11 -0700

    fpga_family/gtx: get rid of hack to pull in non-redistributable files
    We never wanted this in the first place
    Also assert it's not currently used (CI will let me know if I'm wrong)

--- fpga_family/rules.mk
+++ fpga_family/rules.mk
@@ -1,24 +1,5 @@
 GTX_DIR = $(FPGA_FAMILY_DIR)/gtx
 
-# Rules to fetch external files from EXT_FILE_DEPOT
-# Each file, f, to be downloaded must have a corresponding f.remote file
-# in REMOTE_SRC_DIRS and contain the following entry: SHA256 [SHA256 Sum]
-
-# Defaults to HTTP server on local machine if not set externally
-EXT_FILE_DEPOT ?= "http://localhost:8000"
-
-REMOTE_SRC_DIRS = $(GTX_DIR)
-
-REMOTE_TGTS = $(patsubst %.remote, %.v, $(foreach dir, $(REMOTE_SRC_DIRS), $(wildcard $(dir)/*.remote)))
-
-vpath %.remote $(REMOTE_SRC_DIRS)
-
-%.v: %.remote
-	@wget $(EXT_FILE_DEPOT)/$(notdir $@)
-	@touch $(notdir $@)
-	@mv $(notdir $@) $(dir $<)/
-	@grep -q SHA256 $< && cat $< | cut -d' ' -f2 | xargs -I {} echo "{} $(basename $<).v" | sha256sum -c
-
 # Rules to produce macro-expanded q{0,1,2,3}_gtx_wrap components
 
 vpath %.v $(GTX_DIR)
