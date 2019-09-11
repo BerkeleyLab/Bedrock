358b8d5d65c19c474c1205f052b1e4cd2e1926cf
Sergio Paiagua <smferreirapaiagua@lbl.gov>
2019-09-10 18:06:21 -0700

    Adding burst-mode support to lbus_access.py. Enabling new mode on serial-ethernet bridge.
    We still wish for a way to extract device capabilities automatically. A hook (_burst_avail()) was left
    in place to allow this functionality to be added.
    
    For now, using the new burst-mode requires modifying the force_burst argument to the lbus_access constructor.

--- badger/lbus_access.py
+++ badger/lbus_access.py
@@ -7,32 +7,57 @@ import socket
 import numpy
 import random
 import ast
+import binascii
 be32 = numpy.dtype('>u4')
 
 
 class lbus_access:
-    def __init__(self, host, timeout=1.02, port=803):
+    def __init__(self, host, timeout=1.02, port=803, force_burst=False):
         self.dest = (host, int(port))
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
         self.sock.settimeout(timeout)
+        self.force_burst = force_burst
 
-    def _exchange(self, addrs, values=None, drop_reply=False):
+    def _burst_avail(self,):
+        """Determines if device supports block-transfer/repeat-count
+        """
+        # TODO: Extract device capabilities automatically
+        dev_burst_en = False
+
+        return self.force_burst or dev_burst_en
+
+    def _exchange(self, addrs, values=None, drop_reply=False, burst=False):
         """Exchange a single low level message
         """
 
-        msg = numpy.zeros(2+2*len(addrs), dtype=be32)
+        if not burst:
+            msg = numpy.zeros(2+2*len(addrs), dtype=be32)
+        else:
+            msg = numpy.zeros(2+2+len(addrs), dtype=be32)
+
         msg[0] = random.randint(0, 0xffffffff)
         msg[1] = msg[0] ^ 0xffffffff
 
-        for i, (A, V) in enumerate(zip(addrs, values), 1):
+        if not burst:
+            for i, (A, V) in enumerate(zip(addrs, values), 1):
+                A &= 0x00ffffff
+                if V is None:
+                    A |= 0x10000000
+                msg[2*i] = A
+                msg[2*i+1] = V or 0
+        else:
+            RC = (len(addrs) & 0x00ffffff) | 0x20000000
+            A = addrs[0]
             A &= 0x00ffffff
-            if V is None:
+            if values[0] is None:
                 A |= 0x10000000
-            msg[2*i] = A
-            msg[2*i+1] = V or 0
+            msg[2] = RC
+            msg[3] = A
+            for i, V in enumerate(values, 4):
+                msg[i] = V or 0
 
         tosend = msg.tostring()
-        # print("%s Send (%d) %s", self.dest, len(tosend), repr(tosend))
+        # print("%s Send (%d) %s", self.dest, len(tosend), binascii.hexlify(tosend))
         self.sock.sendto(tosend, self.dest)
 
         if drop_reply:
@@ -40,10 +65,11 @@ class lbus_access:
 
         while True:
             reply, src = self.sock.recvfrom(15000)
-            # print("%s Recv (%d) %s", src, len(reply), repr(reply))
+            # print("%s Recv (%d) %s", src, len(reply), binascii.hexlify(reply))
 
-            if len(reply) % 8:
-                reply = reply[:-(len(reply) % 8)]
+            byte_align = 8 if not burst else 4
+            if len(reply) % byte_align:
+                reply = reply[:-(len(reply) % byte_align)]
 
             if len(tosend) != len(reply):
                 print("Reply truncated %d %d" % (len(tosend), len(reply)))
@@ -53,13 +79,15 @@ class lbus_access:
             if (msg[:2] != reply[:2]).any():
                 print('Ignore reply w/o matching nonce %s %s' % (msg[:2], reply[:2]))
                 continue
-            elif (msg[2::2] != reply[2::2]).any():
+            elif not burst and (msg[2::2] != reply[2::2]).any():
                 print('reply addresses are out of order')
                 continue
-
+            elif burst and (msg[2:3] != reply[2:3]).any():
+                print('Non-matching start address')
+                continue
             break
 
-        ret = reply[3::2]
+        ret = reply[3::2] if not burst else reply[4::1]
         return ret
 
     def exchange(self, addrs, values=None, drop_reply=False):
@@ -68,18 +96,24 @@ class lbus_access:
         """
         addrs = list(addrs)
 
+        consec = False
+        # Check for consecutive addresses if burst mode available
+        if self._burst_avail() and (addrs == list(range(addrs[0], addrs[-1]+1))):
+            consec = True
+
         if values is None:
             values = [None]*len(addrs)
         else:
             values = list(values)
 
         ret = numpy.zeros(len(addrs), be32)
-        for i in range(0, len(addrs), 127):
-            A, B = addrs[i:i+127], values[i:i+127]
+        n_tx = 255 if consec else 127
+        for i in range(0, len(addrs), n_tx):
+            A, B = addrs[i:i+n_tx], values[i:i+n_tx]
 
-            P = self._exchange(A, B, drop_reply=drop_reply)
+            P = self._exchange(A, B, drop_reply=drop_reply, burst=consec)
             if not drop_reply:
-                ret[i:i+127] = P
+                ret[i:i+n_tx] = P
 
         return ret
 
@@ -124,6 +158,6 @@ if __name__ == "__main__":
     s.add_argument('mem', nargs='+', help='baseaddr:size')
     args = p.parse_args()
 
-    lbus_acc = lbus_access(args.address, timeout=args.timeout, port=args.port)
+    lbus_acc = lbus_access(args.address, timeout=args.timeout, port=args.port, force_burst=False)
 
     args.func(lbus_acc, args)
