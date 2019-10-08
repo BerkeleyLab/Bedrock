380c0fac40a83293c19c3fdccc7f434f9f85d7a8
Michael Betz <mbetz@lbl.gov>
2019-10-08 11:08:00 -0700

    cmod hw_test.py: find port by serial number

--- soc/picorv32/project/cmod_a7/hw_test.py
+++ soc/picorv32/project/cmod_a7/hw_test.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 '''
-connect to serial port and speak to cmod
+Hardware in the loop test.
+Connect to serial port and speak to CMOD-A7
+
 usage:
 python3 hw_test.py /dev/ttyUSB1
+
+or:
+python3 hw_test.py 210328A6DA44
 '''
 from sys import argv, exit
 from serial import Serial
+from serial.tools import list_ports
 from time import sleep
+
+def getPort(req):
+    '''
+    req can be /dev/ttyUSB1 or a
+    serial number like 210328A6DA44
+    '''
+    for port in list_ports.comports():
+        if port.device == req or port.serial_number == req:
+            return port
+    raise RuntimeError("Port not found: " + req)
 
 if len(argv) != 2:
     print(__doc__)
     exit(-1)
 
-print('hw_test.py: connecting to CMOD-A7 at', argv[1])
-with Serial(port=argv[1], baudrate=115200, timeout=5) as s:
+p = getPort(argv[1])
+
+print('hw_test.py: connecting to', p, p.serial_number)
+with Serial(port=p.device, baudrate=115200, timeout=5) as s:
     s.write(b'\x14')  # reset picorv
     s.flush()
     sleep(0.25)
