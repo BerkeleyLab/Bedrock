Fixing lo_lut_gen.py to actually output sin+cos. Scaling changes

dsp/lo_lut_gen.py
@@ -23,10 +23,6 @@ def lo_gen(amp=1.0, ph_step="14/33", n_bits=18):
         sin_x = amp*np.sin(2*np.pi*(float(frac_per)/float(full_per))*i)
         cos_x = amp*np.cos(2*np.pi*(float(frac_per)/float(full_per))*i)
 
-        # Scale to fixed-point representation
-        sin_x = sin_x * 2**(n_bits-1)
-        cos_x = cos_x * 2**(n_bits-1)
-
         # To signed binary
         sin_x = np.binary_repr(int(sin_x), width=n_bits)
         cos_x = np.binary_repr(int(cos_x), width=n_bits)
@@ -39,7 +35,7 @@ def lo_gen(amp=1.0, ph_step="14/33", n_bits=18):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate a sin/cos look-up-table (LUT)')
-    parser.add_argument('-a', '--amp', type=float, default=1.0,
+    parser.add_argument('-a', '--amp', type=float, default=131071.1, #  2**17-1
                         help='LO amplitude')
     parser.add_argument('-p', '--ph_step', type=str, default="14/33",
                         help='Phase step as irreducible fraction')
@@ -50,4 +46,4 @@ if __name__ == "__main__":
     sin_lut, cos_lut = lo_gen(args.amp, args.ph_step, args.bits)
 
     vlog_rom("sin_lut.vh", sin_lut, args.bits)
-    vlog_rom("cos_lut.vh", sin_lut, args.bits)
+    vlog_rom("cos_lut.vh", cos_lut, args.bits)