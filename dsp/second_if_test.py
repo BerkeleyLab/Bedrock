aab3c052dfbc91ea2875794daeca6971a1d4b3f4
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2019-11-08 08:55:45 -0800

    second_if_test.py: minor rework
    Simplify and generalize where possible
    Control precision of float prints

--- dsp/second_if_test.py
+++ dsp/second_if_test.py
@@ -7,36 +7,34 @@ fname = sys.argv[1]
 with open(fname, 'r') as f:
     a = [int(x) for x in f.readlines()]
 
-if_out = sys.argv[2]
-if if_out == "145":
-    if_out = 188.6 - 145.0
-elif if_out == "60":
-    if_out = 60.0
-else:
-    print("FAIL: Unsupported IF out frequency")
-    sys.exit(1)
+f_samp = 1320.0/7.0
+if_out = float(sys.argv[2])
+if if_out > 0.5*f_samp:
+    if_out = f_samp - if_out
 
 npt = 132*2
 a = a[300:300+npt]
 ss = np.abs(fft(a))
-dt = 7/(1320)
+dt = 1.0/f_samp
 freq_bins = fftfreq(len(a), d=dt)
 
-if len(sys.argv) > 2 and sys.argv[2] == "plot":
+if len(sys.argv) > 3 and sys.argv[3] == "plot":
     plt.plot(fftshift(freq_bins), fftshift(ss))
+    plt.xlabel("f (MHz)")
+    plt.ylabel("Amplitude")
     plt.show()
 
 
 peak = np.max(ss)
 peak_freq = freq_bins[np.argmax(ss)]
-print("Peak at %f MHz" % peak_freq)
+print("Peak at %.3f MHz" % peak_freq)
 fail = 0
 e_delta = 0.05
 for ix in range(int(npt/2)):
     if ss[ix] > 0.003*peak:  # require spurs less than -50 dBc
-        print("%f MHz: %f" % (freq_bins[ix], ss[ix]))
-        if freq_bins[ix] > if_out + e_delta or freq_bins[ix] < if_out - e_delta:
+        print("%.3f MHz: amplitude %.4e" % (freq_bins[ix], ss[ix]))
+        if abs(freq_bins[ix] - if_out) > e_delta:
             print("FAIL: Unexpected spur", float(if_out))
             fail = 1
-print("PASS" if not fail else "FAIL")
+print("FAIL" if fail else "PASS")
 sys.exit(fail)
