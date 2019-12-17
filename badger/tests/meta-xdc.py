07b6ab1e57cf9d5b86137511dfbc8f0024c034d5
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2019-12-17 15:27:49 -0800

    Fix scripts and rules to generate marble1.xdc

--- badger/tests/meta-xdc.py
+++ badger/tests/meta-xdc.py
@@ -1,17 +1,22 @@
 # Experimental implementation of a DSL for building an XDC file
 # by remapping pin names from an existing XDC file
 # See marble/pin_map.csv for the first (and currently only) use case.
-import sys
+from sys import argv
+
+if len(argv) < 3:
+    print("usage: $PYTHON %s foo.xdc foo.csv" % argv[0])
+    print("  where foo.csv is the mapping file")
+    exit(1)
 
 proplist = {}
-for ll in open("Marble.xdc").read().splitlines():
+for ll in open(argv[1]).read().splitlines():
     bb = ll.split()
     pin = bb[-1].rstrip("]")
     rest = " ".join(bb[:-1])
     proplist[pin] = rest
 
 literal = False
-for ll in sys.stdin.read().splitlines():
+for ll in open(argv[2]).read().splitlines():
     if ll == "# Literal output follows":
         literal = True
     elif len(ll) == 0 or ll[0] == "#":
