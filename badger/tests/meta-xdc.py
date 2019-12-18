9a1e8d0dd05ceb7bf25b3e0e0ba924508ce52334
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2019-12-18 07:15:13 -0800

    meta-xdc.py: improve handling of FMC pins
    marble1.bit builds now

--- badger/tests/meta-xdc.py
+++ badger/tests/meta-xdc.py
@@ -2,12 +2,34 @@
 # by remapping pin names from an existing XDC file
 # See marble/pin_map.csv for the first (and currently only) use case.
 from sys import argv
+import re
+converter = {"DIFF_HSTL_II_25": ("LVDS_25", "LVCMOS25")}
+
+
+def merge(prefix, vport):
+    # mostly simple but add a weird heuristic for converting FMC pins
+    m1 = re.match("(.*IOSTANDARD *)(\w+)(.*)", prefix)
+    if m1:
+        ios = m1.group(2)
+        if ios in converter:
+            # now figure out if the port name "looks" differntial
+            # XXX will need to add the case of foo_n[1]
+            diff = len(vport) > 3 and vport[-2:] == "_N" or vport[-2:] == "_P"
+            ios2 = converter[ios][0] if diff else converter[ios][1]
+            # print("woo", ios, diff, ios2)
+            # replace it!
+            prefix = m1.group(1) + ios2 + m1.group(3)
+            # Seems like a lot of work, and fragile, but I haven't
+            # thought of a better solution yet.
+    print(prefix + " " + vport + "]")
+
 
 if len(argv) < 3:
     print("usage: $PYTHON %s foo.xdc foo.csv" % argv[0])
     print("  where foo.csv is the mapping file")
     exit(1)
 
+# original .xdc netlist fle
 proplist = {}
 for ll in open(argv[1]).read().splitlines():
     bb = ll.split()
@@ -15,6 +37,7 @@ for ll in open(argv[1]).read().splitlines():
     rest = " ".join(bb[:-1])
     proplist[pin] = rest
 
+# mapping file
 literal = False
 for ll in open(argv[2]).read().splitlines():
     if ll == "# Literal output follows":
@@ -29,6 +52,6 @@ for ll in open(argv[2]).read().splitlines():
     else:
         pa, pb = ll.split()
         if pa in proplist:
-            print(proplist[pa] + " " + pb + "]")
+            merge(proplist[pa], pb)
         else:
             print("wtf")
