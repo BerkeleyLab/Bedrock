af5c76aceb5f1fc8520d7fb593db6ca1d86a16d7
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2019-12-19 12:31:23 -0800

    test_marblemini: add net_slave_check target
    Needed better ramtest support in testcase.py,
      in turn led to un-confusing readout with result; name now unified as result.

--- projects/test_marblemini/testcase.py
+++ projects/test_marblemini/testcase.py
@@ -14,13 +14,13 @@ def read_result(dev, i2c_base=0x040000, result_len=20, run=True):
     # read out "results"
     if result_len > 0:
         addr = range(i2c_base+0x800, i2c_base+0x800+result_len)
-        readout = dev.exchange(addr)
+        result = dev.exchange(addr)
     else:
-        readout = None
+        result = None
     # thaw result buffer, still keep running
     cmd = 2 if run else 0
     dev.exchange([327687], values=[cmd])
-    return readout
+    return result
 
 
 def wait_for_new(dev, timeout=120, sim=False):
@@ -66,7 +66,7 @@ def run_testcase(dev, prog, result_len=20, sim=False, capture=None, stop=False, 
     dev.exchange(addr, values=prog)
     dev.exchange([327687], values=[2])  # run_cmd=1
     wait_for_new(dev, sim=sim)
-    readout = read_result(dev, result_len=result_len)
+    result = read_result(dev, result_len=result_len)
     if stop:
         dev.exchange([327687], values=[0])  # run_cmd=0
     # read out "logic analyzer" data
@@ -86,7 +86,7 @@ def run_testcase(dev, prog, result_len=20, sim=False, capture=None, stop=False, 
         with open(capture, "w") as ofile:
             # 125 MHz clock and twi_q0=5
             produce_vcd(ofile, logic, dw=dw, mtime=mtime, t_step=8*32)
-    return readout
+    return result
 
 
 def print_sfp1(title, val):
@@ -136,46 +136,63 @@ def print_ina219_config(title, a):
     print("%s INA219 config: 0x%4.4X %s" % (title, x, suffix))
 
 
-def print_readout(result, args, poll_only=False):
+def print_result(result, args, poll_only=False):
     if args.debug:
-        print(readout)
+        print(result)
+    if args.ramtest:
+        print("I2C RAM test")
+        template = {0: 0x5a, 1: 0xa5, 2: 0x5a, 3: 0, 32: 0xa5, 33: 0}
+        fault = False
+        for kx in sorted(template.keys()):
+            t = template[kx]
+            v = result[kx]
+            ss = "OK"
+            if t != v:
+                ss = "BAD!"
+                fault = True
+            print("%2.2x == %2.2x ?  %s" % (v, t, ss))
+        if not fault:
+            print("PASS")
+        else:
+            exit(1)
+        return
     if not poll_only:  # init block
         ib = 2*32  # init result memory base, derived from set_resx(2)
-        print_ina219_config("FMC1", readout[ib+1:ib+3])
-        print_ina219_config("FMC2", readout[ib+3:ib+5])
+        print_ina219_config("FMC1", result[ib+1:ib+3])
+        print_ina219_config("FMC2", result[ib+3:ib+5])
         if args.sfp:
             for ix in range(4):
                 pitch = 50
                 hx = ib + 7 + pitch*ix
-                print("SFP%d:  busmux readback %x" % (ix+1, readout[hx]))
-                print_sfp(readout[1+hx:pitch+hx])
+                print("SFP%d:  busmux readback %x" % (ix+1, result[hx]))
+                print_sfp(result[1+hx:pitch+hx])
             for ix in range(2):
                 pitch = 28
                 hx = ib + 207 + pitch*ix
-                a1 = readout[hx:hx+pitch]
+                a1 = result[hx:hx+pitch]
                 print("FMC%d: busmux 0x%2.2X" % (ix+1, a1[0]))
                 print(a1[1:])
     if True:  # polling block
         if args.sfp:
-            wp_bit = readout[0] & 0x80
+            wp_bit = result[0] & 0x80
             ss = "Off" if wp_bit else "On"
             print("Write Protect switch is %s" % ss)
-            sfp_pp = readout[2]*256 + readout[3]  # parallel SFP status via U34
+            sfp_pp = result[2]*256 + result[3]  # parallel SFP status via U34
             sfp_pp1 = [(sfp_pp >> ix*4) & 0xf for ix in [2, 1, 0, 3]]
-            print_ina219("FMC1", readout[4:4+4])
-            print_ina219("FMC2", readout[8:8+4])
+            print_ina219("FMC1", result[4:4+4])
+            print_ina219("FMC2", result[8:8+4])
             for ix in range(4):
                 pitch = 10
                 hx = 16 + pitch*ix
-                a1 = readout[hx:hx+pitch]
+                a1 = result[hx:hx+pitch]
                 print("SFP%d:  0x%X" % (ix+1, sfp_pp1[ix]))
                 print_sfp_z(a1)
         else:
-            wp_bit = readout[32] & 0x80
+            wp_bit = result[32] & 0x80
             ss = "Off" if wp_bit else "On"
             print("Write Protect switch is %s" % ss)
-            print_ina219("FMC1", readout[38:38+4])
-            print_ina219("FMC2", readout[44:44+4])
+            print_ina219("FMC1", result[38:38+4])
+            print_ina219("FMC2", result[44:44+4])
 
 
 if __name__ == "__main__":
@@ -225,14 +242,14 @@ if __name__ == "__main__":
     if args.poll:
         while True:
             wait_for_new(dev, sim=sim)
-            readout = read_result(dev, result_len=args.rlen)
-            print_readout(readout, args, poll_only=True)
+            result = read_result(dev, result_len=args.rlen)
+            print_result(result, args, poll_only=True)
 
     else:
         if args.debug:
             print(prog)
         print("Program size %d/1024" % len(prog))
-        readout = run_testcase(dev, prog, sim=sim, result_len=args.rlen,
+        result = run_testcase(dev, prog, sim=sim, result_len=args.rlen,
                                debug=args.debug,
                                stop=args.stop, capture=args.vcd)
-        print_readout(readout, args)
+        print_result(result, args)
