a0f40ca7673588fee678f3aba2624a014f728c8c
Sergio Paiagua <smferreirapaiagua@lbl.gov>
2020-02-03 00:11:12 -0800

    Making jxj_gate more robust and capable of handling arbitrary input throughput
    Tested on hardware with new qf2_core. jxj_gate_tb updated to exercise some of the
    corner cases observed.
    
    Yet to test with previous qf2_core or bmb7.
    
    serial_io/tx_8b9b.v does not cope well with interrupted tx_stb. Needs updating

--- board_support/bmb7_kintex/jxj_gate_tb.v
+++ board_support/bmb7_kintex/jxj_gate_tb.v
@@ -2,6 +2,9 @@
 
 module jxj_gate_tb;
 
+   localparam RX_SPACING = 1; // TODO: Randomly generate spacing, including 1
+   localparam NUM_WORDS = 32;
+
    localparam max_data_len=1024;
    reg [7:0] pack_mem [0:max_data_len-1];
 
@@ -16,7 +19,7 @@ module jxj_gate_tb;
    initial begin
       use_packfile = $value$plusargs("packet_file=%s", packet_file);
       if (use_packfile) $readmemh(packet_file,pack_mem);
-      if (!$value$plusargs("data_len=%d", data_len))  data_len= 16;
+      if (!$value$plusargs("data_len=%d", data_len))  data_len= NUM_WORDS;
       if ($value$plusargs("udp_port=%d", udp_port))  udp_port=0;
       if ($test$plusargs("vcd")) begin
          $dumpfile("jxj_gate.vcd");
@@ -32,18 +35,17 @@ module jxj_gate_tb;
 
    // Simple test data that reads registers 0 through 3
    localparam WORD_LEN = 8; // bits
-   localparam NUM_WORDS = 16;
    localparam RTEST_LEN = 4;
    reg [0:WORD_LEN*NUM_WORDS-1] rtest_trace[RTEST_LEN-1:0];
    reg [0:WORD_LEN*NUM_WORDS-1] rtest_model[RTEST_LEN-1:0];
    reg [0:31] reg_data[RTEST_LEN-1:0];
    integer i;
    initial begin
-      // NONCE + COMMAND + ADDRESS + DATA
-      rtest_trace[0] = {64'h123456789ABCDEF0, 8'h10, 24'h000000, 32'h00000000};
-      rtest_trace[1] = {64'hFEDCBA9876543210, 8'h10, 24'h000001, 32'h00000000};
-      rtest_trace[2] = {64'h123456789ABCDEF0, 8'h10, 24'h000002, 32'h00000000};
-      rtest_trace[3] = {64'hFEDCBA9876543210, 8'h10, 24'h000003, 32'h00000000};
+      // NONCE + (COMMAND0 + ADDRESS0) + DATA0 + (COMMAND1 + ADDRESS1) + DATA1
+      rtest_trace[0] = {64'h123456789ABCDEF0, 32'h00000000, 32'h00000001, 32'h00000001, 32'h00000002, 32'h00000002, 32'h00000003}; // Write
+      rtest_trace[1] = {64'hFEDCBA9876543210, 32'h10000000, 32'h00000000, 32'h10000001, 32'h00000000, 32'h10000002, 32'h00000000};
+      rtest_trace[2] = {64'h123456789ABCDEF0, 32'h10000000, 32'h00000000, 32'h10000001, 32'h00000000, 32'h10000002, 32'h00000000};
+      rtest_trace[3] = {64'hFEDCBA9876543210, 32'h10000000, 32'h00000000, 32'h10000001, 32'h00000000, 32'h10000002, 32'h00000000};
       // Expected response
       reg_data[0] = "Hell";
       reg_data[1] = "o wo";
@@ -51,7 +53,10 @@ module jxj_gate_tb;
       reg_data[3] = 32'h0d0a0d0a;
       // Expected stream
       for (i=0; i<RTEST_LEN-1; i++) begin
-         rtest_model[i] = {rtest_trace[i][0:95], reg_data[i]};
+         rtest_model[i] = rtest_trace[i];
+         rtest_model[i][96:127] = reg_data[0];
+         rtest_model[i][160:191] = reg_data[1];
+         rtest_model[i][224:255] = reg_data[2];
       end
    end
 
@@ -66,6 +71,7 @@ module jxj_gate_tb;
 
    // Local bus
    wire [23:0] lb_addr;
+   reg [23:0] lb_addr_r;
    wire [31:0] lb_dout;
    reg [31:0] lb_din;
    wire lb_strobe, lb_rd;
@@ -78,25 +84,32 @@ module jxj_gate_tb;
    );
 
    // Simulate Rx data
-   integer ci;
+   integer ci=0;
    integer reg_idx=0;
    integer wrd_idx=0;
+   integer skip;
    always @(posedge clk) begin
-      ci = cc % (data_len*9+150);
-      if ((ci>=100) & (ci<(100+data_len*9)) & (ci%9==0)) begin
-         rx_stb <= 1;
-         if (use_packfile)
-            rx_din <= pack_mem[(ci-100)/9];
-         else
-            rx_din <= rtest_trace[reg_idx][WORD_LEN*wrd_idx+: WORD_LEN];
+      rx_din <= 8'hxx;
+      rx_stb <= 0;
+      rx_end <= 0;
 
-         wrd_idx <= (wrd_idx + 1) % NUM_WORDS;
-         reg_idx <= (&wrd_idx) ? reg_idx + 1 : reg_idx;
-      end else begin
-         rx_din <= 8'hxx;
-         rx_stb <= 0;
+      skip = $urandom % 100;
+      if (skip < 80) begin
+         ci = (ci+1) % (data_len*RX_SPACING+150);
+         if ((ci>=100) & (ci<(100+data_len*RX_SPACING)) & (ci%RX_SPACING==0)) begin
+            rx_stb <= 1;
+            if (use_packfile) begin
+               rx_din <= pack_mem[(ci-100)/RX_SPACING];
+               rx_end <= (ci==(99+data_len*RX_SPACING));
+            end else begin
+               rx_din <= rtest_trace[reg_idx][WORD_LEN*wrd_idx+: WORD_LEN];
+
+               rx_end  <= (wrd_idx==(NUM_WORDS-1));
+               wrd_idx <= (wrd_idx + 1) % NUM_WORDS;
+               reg_idx <= (wrd_idx==(NUM_WORDS-1)) ? reg_idx + 1 : reg_idx;
+            end
+         end
       end
-         rx_end <= (ci==(99+data_len*9));
    end
 
    `define NO_TX
@@ -108,7 +121,7 @@ module jxj_gate_tb;
       tx_cnt <= tx_cnt==8 ? 0 : tx_cnt+1;
       tx_stb_r <= (tx_cnt==0) & tx_rdy;
    end
-   assign tx_stb = 1; // tx_stb_r;
+   assign tx_stb = 1; //tx_stb_r;
    `else
    wire data_out_pin;
    // tx_8b9b drives tx_stb
@@ -139,12 +152,17 @@ module jxj_gate_tb;
    `endif
 
    // Simple test rig
-   always @(posedge clk) case (lb_addr[2:0])
+   always @(posedge clk) begin
+      if (lb_strobe)
+         lb_addr_r <= lb_addr;
+
+      case (lb_addr_r[2:0])
       0: lb_din <= reg_data[0];
       1: lb_din <= reg_data[1];
       2: lb_din <= reg_data[2];
       3: lb_din <= reg_data[3];
-   endcase
+      endcase
+   end
 
    // Simple scoreboard based on simple test rig and register trace
 
@@ -158,8 +176,16 @@ module jxj_gate_tb;
             $display("%t ERROR. Expected %x, got %x", $time, wrd_model, tx_dout);
             fail <= 1;
          end
+         if (tx_end && w_idx != NUM_WORDS-1) begin
+            $display("%t ERROR. Unexpected tx_end", $time);
+            fail <= 1;
+         end
+         if (!tx_end && w_idx == NUM_WORDS-1) begin
+            $display("%t ERROR. Expected tx_end", $time);
+            fail <= 1;
+         end
          w_idx <= (w_idx + 1) % NUM_WORDS;
-         r_idx <= (&w_idx) ? r_idx + 1 : r_idx;
+         r_idx <= (w_idx==(NUM_WORDS-1)) ? r_idx + 1 : r_idx;
       end
    end
 
