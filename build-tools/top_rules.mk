b365d92d35d35809ddcbedf449fc7afa2b1aacd6
Sergio Paiagua <smferreirapaiagua@lbl.gov>
2020-04-27 19:54:46 -0700

    Adding basic SV support to newad and corresponding rules to top-rules.mk

--- build-tools/top_rules.mk
+++ build-tools/top_rules.mk
@@ -179,14 +179,39 @@ SPACE := $(EMPTY) $(EMPTY)
 COMMA := ,
 NEWAD_DIRS = .
 NEWAD_ARGS = -d $(subst $(SPACE),$(COMMA),$(NEWAD_DIRS)) -i $< -w $(LB_AW)
+
+define NEWAD_O
+mkdir -p $(AUTOGEN_DIR); $(PYTHON) $(BUILD_DIR)/newad.py -o $@ $(NEWAD_ARGS) $(NEWAD_ARGS_$*)
+endef
+define NEWAD_A
+mkdir -p $(AUTOGEN_DIR); $(PYTHON) $(BUILD_DIR)/newad.py -a $@ $(NEWAD_ARGS) $(NEWAD_ARGS_$*)
+endef
+define NEWAD_L
+mkdir -p $(AUTOGEN_DIR); $(PYTHON) $(BUILD_DIR)/newad.py -l -r $@ $(NEWAD_ARGS) $(NEWAD_ARGS_$*)
+endef
+define REV_JSON
+$(PYTHON) $(BUILD_DIR)/reverse_json.py $< > $@
+endef
+
+$(AUTOGEN_DIR)/%_auto.vh: %.sv
+	$(NEWAD_O)
 $(AUTOGEN_DIR)/%_auto.vh: %.v
-	mkdir -p $(AUTOGEN_DIR); $(PYTHON) $(BUILD_DIR)/newad.py -o $@ $(NEWAD_ARGS) $(NEWAD_ARGS_$*)
+	$(NEWAD_O)
 
+$(AUTOGEN_DIR)/addr_map_%.vh: %.sv
+	$(NEWAD_A)
 $(AUTOGEN_DIR)/addr_map_%.vh: %.v
-	mkdir -p $(AUTOGEN_DIR); $(PYTHON) $(BUILD_DIR)/newad.py -a $@ $(NEWAD_ARGS) $(NEWAD_ARGS_$*)
+	$(NEWAD_A)
 
+$(AUTOGEN_DIR)/regmap_%.json: %.sv
+	$(NEWAD_L)
 $(AUTOGEN_DIR)/regmap_%.json: %.v
-	mkdir -p $(AUTOGEN_DIR); $(PYTHON) $(BUILD_DIR)/newad.py -l -r $@ $(NEWAD_ARGS) $(NEWAD_ARGS_$*)
+	$(NEWAD_L)
+
+$(AUTOGEN_DIR)/scalar_%_regmap.json: %.sv
+	$(REV_JSON)
+$(AUTOGEN_DIR)/scalar_%_regmap.json: %.v
+	$(REV_JSON)
 
 # http://www.graphviz.org/content/dot-language
 # apt-get install graphviz
