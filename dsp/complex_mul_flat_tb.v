c647350164d0393edfcb309ab729ee45e5ccb7cf
Vamsi K Vytla <vkvytla@lbl.gov>
2020-07-22 14:57:41 -0700

    complex_mul_flat.v: x+iy * a+ib with two separate i and q streams multiply_accumulate.v: Perhaps another one, but definitely more generic non_iq_interleaved_piloop.v: Yet another piloop. But with flat I-Q.    This could use better testing, and a rename (piloop_flat_iq?).

---
+++ dsp/complex_mul_flat_tb.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ns
+
+module complex_mul_tb;
+
+reg clk;
+integer cc;
+reg fail=0;
+initial begin
+	if ($test$plusargs("vcd")) begin
+		$dumpfile("complex_mul_flat.vcd");
+		$dumpvars(5,complex_mul_tb);
+	end
+	for (cc=0; cc<50; cc=cc+1) begin
+		clk=0; #5;
+		clk=1; #5;
+	end
+	$display("%s", fail ? "FAIL" : "PASS");
+	$finish();
+end
+
+reg signed [17:0] x_I=0, x_Q=0, y_I=0, y_Q=0;
+wire signed [17:0] z_I, z_Q;
+reg signed [35:0] fi=0, fq=0, fi_d=0, fq_d=0, fi_d2=0, fq_d2=0;  // reference results
+always @(posedge clk) begin
+	x_I <= $random;
+	x_Q <= $random;
+	y_I <= $random;
+	y_Q <= $random;
+	fi <= x_I * y_I - x_Q * y_Q;
+	fq <= x_I * y_Q + y_I * x_Q;
+	fi_d <= fi;
+	fq_d <= fq;
+	fi_d2 <= fi_d;
+	fq_d2 <= fq_d;
+end
+
+wire g_out;
+complex_mul_flat dut(.clk(clk), .gate_in(cc%4 == 0),
+	.x_I(x_I), .x_Q(x_Q),
+	.y_I(y_I), .y_Q(y_Q),
+	.z_I(z_I), .z_Q(z_Q), .gate_out(g_out));
+
+reg signed [35:0] fi_test, fq_test, zx_I, zx_Q;
+reg fault=0;
+always @(negedge clk) if (cc>6 && g_out) begin
+	zx_I = z_I * 131072;
+	zx_Q = z_Q * 131072;
+	fi_test = fi_d2;
+	fq_test = fq_d2;
+	if (fi_test >  131072*131071) fi_test =  131072*131071;
+	if (fi_test < -131072*131072) fi_test = -131072*131072;
+	if (fq_test >  131072*131071) fq_test =  131072*131071;
+	if (fq_test < -131072*131072) fq_test = -131072*131072;
+	fault = (zx_I > fi_test+131072 || zx_I < fi_test-131072 ||
+		zx_Q > fq_test+131072  || zx_Q < fq_test-131072);
+	if (fault) fail=1;
+	$display("%d %d %d %d %d %d %s\n",
+	 zx_I, fi_test, zx_I - fi_test,
+	 zx_Q, fq_test, zx_Q - fq_test, fault ? "FAULT" : "    .");
+end
+
+endmodule
---
+++ dsp/non_iq_interleaved_piloop_tb.v
@@ -0,0 +1,68 @@
+// Test bench taken from piloop2_tb.v.
+`timescale 1ns / 1ns
+
+module non_iq_interleaved_piloop_tb;
+
+reg clk;
+integer cc;
+reg trace, fail;
+integer out_fd;
+initial begin
+	if ($test$plusargs("vcd")) begin
+		$dumpfile("non_iq_interleaved_piloop.vcd");
+		$dumpvars(5, non_iq_interleaved_piloop_tb);
+	end
+	trace = $test$plusargs("trace");
+	out_fd = $fopen("non_iq_interleaved_piloop.out", "w");
+	$fwrite(out_fd, "# cc kp ki x_i x_q setpoint_i setpoint_q y_i y_q\n");
+	for (cc=0; cc<4000; cc=cc+1) begin
+		clk=0; #5;
+		clk=1; #5;
+	end
+	$finish();
+end
+
+reg signed [17:0] xin=0, setpoint=5000;
+reg [17:0] kp=0,ki=0,static_set=1000;
+always @(posedge clk) begin
+	if (cc ==  250)  kp <=  50000;
+	if (cc ==  500)  kp <= 100000;
+	if (cc == 1000) xin <= 1500;
+	if (cc == 1300)  ki <= 10000;
+	if (cc == 3500) xin <= 600;
+	if (cc == 3900) xin <= 1000;
+end
+
+wire str_out;
+wire signed [17:0] corr_i, corr_q;
+reg [3:0] post_mult_shift=0;
+
+reg signed [18:0] err_i=0, err_q=0;
+reg reverse=0;
+
+
+always @ (posedge clk) begin
+	err_i <= reverse ? xin - setpoint : setpoint - xin;
+	err_q <= reverse ? xin - setpoint : setpoint - xin;
+end
+
+non_iq_interleaved_piloop piloop (.clk(clk),
+	.Kp_I(kp), .Kp_Q(kp), .Ki_over_Kp(ki),
+	.err_i(err_i), .err_q(err_q),
+	.feedback_enable(1'b1),
+	.integrator_enable(1'b1),
+	.post_mult_shift(post_mult_shift),
+	.fdfwd_i(17'b0),
+	.fdfwd_q(17'b0),
+	.pi_out_i(corr_i),
+	.pi_out_q(corr_q)
+);
+
+always @(negedge clk) if (out_fd) begin
+	#1; $fwrite(out_fd, "%d %d %d %d %d %d %d %d %d\n",
+		    cc, kp, ki, xin, xin,
+		    setpoint, setpoint,
+		    corr_i, corr_q);
+end
+
+endmodule
