0703393bd8f3682e01cbc2bef0a9e431978c78fb
Michael Betz <mbetz@lbl.gov>
2020-07-29 23:42:11 -0700

    spi.h: add SPI_SEND_N, which transmits N bits

--- soc/picorv32/firmware/inc/spi.h
+++ soc/picorv32/firmware/inc/spi.h
@@ -13,10 +13,10 @@
 #define BIT_CPOL     16   // Clock polarity: 0 = idle low, 1 = idle high
 #define BIT_CPHA     17   // Clock phase: 0 = sample on first edge, 1 = second edge
 #define BIT_LSB      18   // When set, transmit LSB first, otherwise MSB
-#define BIT_SS_MAN   25   // when set, the SS pin can be controlled through cfg_ss_ctrl
-#define BIT_SS_CTRL  26   // Manually control state of SS pin
+#define BIT_CS_MAN   25   // when set, the CS pin can be controlled through cfg_ss_ctrl
+#define BIT_CS_CTRL  26   // Manually control state of CS pin
 #define BIT_BUSY     30   // set when spi_engine is busy
-#define BIT_MISO     31   // Status of MISO
+#define BIT_CIPO     31   // Status of CIPO
 
 // Initialize the SPI master
 #define SPI_INIT(                                                          \
@@ -24,8 +24,8 @@
   ) SET_REG (                                                              \
     spi_base_addr+(SPI_CFG_REG<<2),                                        \
     (clk_div)<<BIT_CLK_DIV | (cpol)<<BIT_CPOL   | (cpha)<<BIT_CPHA     |   \
-    (lsb)<<BIT_LSB         | (nbits)<<BIT_NBITS | (ss_man)<<BIT_SS_MAN |   \
-    (ss_ctrl)<<BIT_SS_CTRL                                                 \
+    (lsb)<<BIT_LSB         | (nbits)<<BIT_NBITS | (ss_man)<<BIT_CS_MAN |   \
+    (ss_ctrl)<<BIT_CS_CTRL                                                 \
   )
 
 // Returns the config word (initialized by SPI_INIT())
@@ -34,11 +34,11 @@
 
 // Returns 1 if a transmission is in progress, otherwise 0
 #define SPI_IS_BUSY(spi_base_addr) \
-  GET_SFR1( spi_base_addr, SPI_CFG_REG, BIT_BUSY )
+  GET_SFR1(spi_base_addr, SPI_CFG_REG, BIT_BUSY )
 
-// Set the SS pin high / low (BIT_SS_CTRL must be set)
-#define SPI_SS_SET( spi_base_addr, val ) \
-  SET_SFR1( spi_base_addr, SPI_CFG_REG, BIT_SS_CTRL, val )
+// Set the CS pin high / low (BIT_CS_CTRL must be set)
+#define SPI_CS_SET(spi_base_addr, val ) \
+  SET_SFR1(spi_base_addr, SPI_CFG_REG, BIT_CS_CTRL, val )
 
 // Returns the last received data word
 #define SPI_GET_DAT(spi_base_addr) \
@@ -52,6 +52,13 @@
 #define SPI_SET_DAT_BLOCK(spi_base_addr, val) { \
   SPI_SET_DAT(spi_base_addr, val);              \
   while(SPI_IS_BUSY(spi_base_addr));            \
+}
+
+// transmit N (1 - 32) bits of val
+#define SPI_SEND_N(spi_base, N, val) { \
+  while(SPI_IS_BUSY(spi_base)); \
+  SET_REG8(spi_base + SPI_CFG_REG * 4 + 1, N); \
+  SPI_SET_DAT(spi_base, val); \
 }
 
 #endif
