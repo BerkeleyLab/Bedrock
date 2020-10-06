8b9e48f647d191016cab0bb2afb9268bc02bcad3
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2020-10-06 11:28:08 -0700

    Refresh tinyEVR from wenorum

--- dsp/tinyEVR.v
+++ dsp/tinyEVR.v
@@ -20,24 +20,114 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+`timescale 1ns / 1ns
+
+////////////////////////////////////////////////////////////////////////////////
 // Very small subset of MRF event receiver
 // Provides time stamps, event strobes and some status markers.
-
 module tinyEVR #(
-    parameter EVSTROBE_COUNT  = 126,
-    parameter TIMESTAMP_WIDTH = 64,
-    parameter DEBUG           = "false"
+    parameter EVSTROBE_COUNT   = 126,
+    parameter DEBUG            = "false",
+    parameter TIMESTAMP_WIDTH  = 64
     ) (
     input  wire                                            evrRxClk,
 
-    (*mark_debug=DEBUG*) input  wire                [15:0] evrRxWord,
-    (*mark_debug=DEBUG*) input  wire                 [1:0] evrCharIsK,
+    (*mark_debug=DEBUG*) input                      [15:0] evrRxWord,
+    (*mark_debug=DEBUG*) input                       [1:0] evrCharIsK,
+
+    (*mark_debug=DEBUG*) output wire                       ppsMarker,
+    (*mark_debug=DEBUG*) output wire                       timestampValid,
+    (*mark_debug=DEBUG*) output wire [TIMESTAMP_WIDTH-1:0] timestamp,
+    (*mark_debug=DEBUG*) output wire                 [7:0] distributedDataBus,
+    output wire [EVSTROBE_COUNT:1]                         evStrobe);
+
+tinyEVRcommon #(.ACTION_RAM_WIDTH(0),
+                .EVSTROBE_COUNT(EVSTROBE_COUNT),
+                .DEBUG(DEBUG),
+                .TIMESTAMP_WIDTH(TIMESTAMP_WIDTH))
+  tinyEVRcommon (
+    .evrRxClk(evrRxClk),
+    .evrRxWord(evrRxWord),
+    .evrCharIsK(evrCharIsK),
+    .ppsMarker(ppsMarker),
+    .timestampValid(timestampValid),
+    .timestamp(timestamp),
+    .distributedDataBus(distributedDataBus),
+    .action(evStrobe),
+    .sysClk(1'b0),
+    .sysActionWriteEnable(1'b0),
+    .sysActionAddress(8'h00),
+    .sysActionData(1'b0));
+endmodule
+
+////////////////////////////////////////////////////////////////////////////////
+// Somewhat larger subset of MRF event receiver
+// Based on more conventional lookup table
+module smallEVR #(
+    parameter ACTION_WIDTH    = 1,
+    parameter DEBUG           = "false",
+    parameter TIMESTAMP_WIDTH = 64
+    ) (
+    input  wire                                            evrRxClk,
+
+    (*mark_debug=DEBUG*) input                      [15:0] evrRxWord,
+    (*mark_debug=DEBUG*) input                       [1:0] evrCharIsK,
+
+    (*mark_debug=DEBUG*) output wire                       ppsMarker,
+    (*mark_debug=DEBUG*) output wire                       timestampValid,
+    (*mark_debug=DEBUG*) output wire [TIMESTAMP_WIDTH-1:0] timestamp,
+    (*mark_debug=DEBUG*) output wire                 [7:0] distributedDataBus,
+    (*mark_debug=DEBUG*) output wire    [ACTION_WIDTH-1:0] action,
+
+    input                    sysClk,
+    input                    sysActionWriteEnable,
+    input              [7:0] sysActionAddress,
+    input [ACTION_WIDTH-1:0] sysActionData);
+
+tinyEVRcommon #(.ACTION_RAM_WIDTH(ACTION_WIDTH),
+                .EVSTROBE_COUNT(0),
+                .DEBUG(DEBUG),
+                .TIMESTAMP_WIDTH(TIMESTAMP_WIDTH))
+  tinyEVRcommon (
+    .evrRxClk(evrRxClk),
+    .evrRxWord(evrRxWord),
+    .evrCharIsK(evrCharIsK),
+    .ppsMarker(ppsMarker),
+    .timestampValid(timestampValid),
+    .timestamp(timestamp),
+    .distributedDataBus(distributedDataBus),
+    .action(action),
+    .sysClk(sysClk),
+    .sysActionWriteEnable(sysActionWriteEnable),
+    .sysActionAddress(sysActionAddress),
+    .sysActionData(sysActionData));
+endmodule
+
+////////////////////////////////////////////////////////////////////////////////
+// Common implementation
+module tinyEVRcommon #(
+    parameter ACTION_RAM_WIDTH = 0,
+    parameter EVSTROBE_COUNT   = 126,
+    parameter DEBUG            = "false",
+    parameter TIMESTAMP_WIDTH  = 64,
+    parameter ACT_MSB = ACTION_RAM_WIDTH?ACTION_RAM_WIDTH-1:EVSTROBE_COUNT,
+    parameter ACT_LSB = ACTION_RAM_WIDTH?0:1
+    ) (
+    input  wire                                            evrRxClk,
+
+    (*mark_debug=DEBUG*) input                      [15:0] evrRxWord,
+    (*mark_debug=DEBUG*) input                       [1:0] evrCharIsK,
 
     (*mark_debug=DEBUG*) output wire                       ppsMarker,
     (*mark_debug=DEBUG*) output reg                        timestampValid = 0,
     (*mark_debug=DEBUG*) output wire [TIMESTAMP_WIDTH-1:0] timestamp,
-    (*mark_debug=DEBUG*) output reg     [EVSTROBE_COUNT:1] evStrobe = 0,
-                         output wire                 [7:0] distributedDataBus);
+    (*mark_debug=DEBUG*) output wire                 [7:0] distributedDataBus,
+    output wire [ACT_MSB:ACT_LSB]                          action,
+
+    input                                             sysClk,
+    input                                             sysActionWriteEnable,
+    input                                       [7:0] sysActionAddress,
+    input [(ACTION_RAM_WIDTH?ACTION_RAM_WIDTH-1:0):0] sysActionData);
 
 localparam SECONDS_WIDTH = TIMESTAMP_WIDTH/2;
 localparam TICKS_WIDTH   = TIMESTAMP_WIDTH/2;
@@ -90,15 +180,44 @@ always @(posedge evrRxClk) begin
     end
 end
 
-// Generate event strobes
-// Rely on synthesis optimizer to remove unused outputs
-assign ppsMarker = evStrobe[EVCODE_SECONDS_MARKER];
+// Rely on the optimizer to clean out all unused event strobes
 genvar e;
-generate
-for (e = 1 ; e <= EVSTROBE_COUNT ; e = e + 1) begin : evstr
+wire [254:1] evStrobe;
+assign ppsMarker = evStrobe[EVCODE_SECONDS_MARKER];
+for (e = 1 ; e <= 254 ; e = e + 1) begin : evstr
+    reg evs;
     always @(posedge evrRxClk) begin
-        evStrobe[e] <= (evCodeValid && (evCode == e));
+        evs <= (evCodeValid && (evCode == e));
     end
+    assign evStrobe[e] = evs;
+end
+
+generate
+if (ACTION_RAM_WIDTH > 0) begin
+ //
+ // Traditional lookup-table based actions
+ //
+ reg [ACTION_RAM_WIDTH-1:0] actionRAM [0:255], actionRAMQ, actionBus;
+ reg ramQisValid = 0;
+ always @(posedge sysClk) begin
+    if (sysActionWriteEnable) begin
+        actionRAM[sysActionAddress] <= sysActionData;
+    end
+ end
+ always @(posedge evrRxClk) begin
+    ramQisValid <= evCodeValid;
+    actionRAMQ <= actionRAM[evCode];
+    actionBus <= ramQisValid ? actionRAMQ : 0;
+ end
+ assign action = actionBus;
+end
+else begin
+ //
+ // Individual strobes per event
+ //
+ for (e = 1 ; e <= EVSTROBE_COUNT ; e = e + 1) begin : evact
+    assign action[e] = evStrobe[e];
+ end
 end
 endgenerate
 
