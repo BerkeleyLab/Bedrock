8f847fa4d842f590f6bcf33c53d24b4accaa3c12
Michael Betz <mbetz@lbl.gov>
2020-11-19 14:34:49 -0800

    scan_vcxo.py: fix imnport path, PEP8

--- projects/test_marblemini/scan_vcxo.py
+++ projects/test_marblemini/scan_vcxo.py
@@ -3,7 +3,7 @@ import argparse
 import sys
 import os
 import time
-sys.path.append(os.path.join(os.path.dirname(__file__), "bedrock/badger"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../badger"))
 from lbus_access import lbus_access
 
 
@@ -19,7 +19,7 @@ def measure_1(chip, v, dac=2, pause=1.1, repeat=1, gps=False, verbose=False):
         print("Invalid DAC choice")
         exit(1)
     if gps:
-        pause = 0.3*pause
+        pause = 0.3 * pause
     chip.exchange([327689], [v])
     ppm = []
     oldn = None
@@ -30,16 +30,18 @@ def measure_1(chip, v, dac=2, pause=1.1, repeat=1, gps=False, verbose=False):
             n = (raw >> 28) & 0xf
             ovf = (raw >> 27) & 0x1
             count = raw & 0x7ffffff
-            ok = oldn is not None and n == ((oldn+1) & 0xf) and not ovf
+            ok = oldn is not None and n == ((oldn + 1) & 0xf) and not ovf
             if verbose and oldn is not None:
-                print("chk %x %x %d %9d %s" % (oldn, n, ovf, count, "OK" if ok else "."))
+                print("chk %x %x %d %9d %s" % (
+                    oldn, n, ovf, count, "OK" if ok else ".")
+                )
             if ok:
-                x = (float(count)/125000000.0-1.0)*1e6
+                x = (float(count) / 125000000.0 - 1.0) * 1e6
                 ppm += [x]
             oldn = n
         else:
             raw = chip.exchange([5])
-            ppm += [(float(raw)/2**27-1.0)*1e6]
+            ppm += [(float(raw) / 2**27 - 1.0) * 1e6]
     return ppm
 
 
@@ -67,10 +69,17 @@ if __name__ == "__main__":
     plot1 = []
     plot2 = []
     for jx in range(0, 17):
-        v = min(jx*4096, 65535)
-        ppm = measure_1(chip, v, dac=int(args.dac), repeat=4, gps=args.gps, verbose=args.verbose)
+        v = min(jx * 4096, 65535)
+        ppm = measure_1(
+            chip,
+            v,
+            dac=int(args.dac),
+            repeat=4,
+            gps=args.gps,
+            verbose=args.verbose
+        )
         print("%5d  %+.3f %+.3f %+.3f ppm" % (v, ppm[1], ppm[2], ppm[3]))
-        plx += [float(v)/65535]
+        plx += [float(v) / 65535]
         plot1 += [ppm[1]]
         plot2 += [ppm[2]]
         sys.stdout.flush()
