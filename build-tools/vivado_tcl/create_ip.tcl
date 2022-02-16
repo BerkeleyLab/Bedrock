c362972d03829722367fe4d23edd1c8e51fac382
Lucas Russo <lerwys@gmail.com>
2022-02-16 12:01:52 -0800

    build-tools: re-add IP tcl scripts from "build" repository
    On consolidation from various repos to bedrock
    these scripts were left out, but they are useful
    and will be used by Xilinx projects requiring IP
    creation.

---
+++ build-tools/vivado_tcl/create_ip.tcl
@@ -0,0 +1,17 @@
+# Usage: "vivado -mode batch -source create_ip.tcl -tclargs <source_files>"
+if { $argc <1 } {
+    puts "Not enough arguments"
+    puts "Usage: vivado -mode batch -source create_ip.tcl -tclargs <ip_name> <ip_path> <source_files>"
+    exit
+}
+
+set ip_name [lindex $argv 0]
+set ip_path [lindex $argv 1]
+set my_ip_files [lrange $argv 2 end]
+
+ip_create $ip_name $ip_path
+ip_files $ip_name $my_ip_files
+update_compile_order -fileset sources_1
+ip_properties $ip_name
+
+ipx::save_core [ipx::current_core]
