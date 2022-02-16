c5a960cde6a3df3ededb1ee9977c0f05511c8384
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2022-02-16 12:24:14 -0800

    half_filt.v: churn
    put whitespace back the way it was
    keep data path from depending at all on reset signal

--- dsp/half_filt.v
+++ dsp/half_filt.v
@@ -2,12 +2,12 @@
 `timescale 1ns / 1ns
 
 module half_filt(
-    input clk,  // Rising edge clock input; all logic is synchronous in this domain
-    input signed [19:0] ind,  // Input data
-    input ing,  // Active high gate marking input data as valid
-    output signed [19:0] outd,  // Output data
-    output outg,  // Active high gate marking output data as valid
-    input reset  // manually reset counter
+	input clk,  // Rising edge clock input; all logic is synchronous in this domain
+	input signed [19:0] ind,  // Input data
+	input ing,  // Active high gate marking input data as valid
+	output signed [19:0] outd,  // Output data
+	output outg,  // Active high gate marking output data as valid
+	input reset  // manually reset counter
 );
 
 parameter len = 4;  // number of interleaved data streams
@@ -28,15 +28,15 @@ reg_delay #(.dw(20),.len(len)) h9(clk, 1'b0, ing, d9, d10);
 reg signed [20:0] s1=0, s2=0, s3=0, s4=0;
 reg sg=0;
 always @(posedge clk) begin
-    if (reset) begin
-        sg <= 0;
-    end else begin
-        s1 <= d0 + d10;
-        s2 <= d2 + d8;
-        s3 <= d4 + d6;
-        s4 <= d5 + 0; // {d5[19],d5};
-        sg <= ing;
-    end
+	if (reset) begin
+		sg <= 0;
+	end else begin
+		sg <= ing;
+	end
+	s1 <= d0 + d10;
+	s2 <= d2 + d8;
+	s3 <= d4 + d6;
+	s4 <= d5 + 0; // {d5[19],d5};
 end
 
 // FIR filter coefficients:
@@ -46,22 +46,22 @@ end
 reg signed [21:0] a1=0, a2=0, a3=0, a4=0;
 reg ag=0;
 always @(posedge clk) begin
-    if (reset) begin
-        ag <= 0;
-    end else begin
-        `ifdef TRUST_VERILOG_DIVISION
-            a1 <= s1/16 + 0;
-            a2 <= s2/4 + s2/32;
-            a3 <= s3/4 - s3/32;
-            a4 <= s3 + 2*s4;
-        `else
-            a1 <= {{5{s1[20]}},s1[20:4]};
-            a2 <= {{3{s2[20]}},s2[20:2]} + {{6{s2[20]}},s2[20:5]};
-            a3 <= {{3{s3[20]}},s3[20:2]} - {{6{s3[20]}},s3[20:5]};
-            a4 <= {{1{s3[20]}},s3[20:0]} + {            s4[20:0],1'b0};
-        `endif
-        ag <= sg;
-end
+	if (reset) begin
+		ag <= 0;
+	end else begin
+		ag <= sg;
+	end
+`ifdef TRUST_VERILOG_DIVISION
+	a1 <= s1/16 + 0;
+	a2 <= s2/4 + s2/32;
+	a3 <= s3/4 - s3/32;
+	a4 <= s3 + 2*s4;
+`else
+	a1 <= {{5{s1[20]}},s1[20:4]};
+	a2 <= {{3{s2[20]}},s2[20:2]} + {{6{s2[20]}},s2[20:5]};
+	a3 <= {{3{s3[20]}},s3[20:2]} - {{6{s3[20]}},s3[20:5]};
+	a4 <= {{1{s3[20]}},s3[20:0]} + {            s4[20:0],1'b0};
+`endif
 end
 
 reg signed [22:0] b1=0, b2=0;
@@ -69,30 +69,28 @@ reg bg=0;
 reg [8:0] samp=0;
 reg show=0;
 always @(posedge clk) begin
-    if (reset) begin
-        bg <= 0;
-        samp <= 0;
-        show <= 0;
-    end else begin
-        b1 <= a1 - a2;
-        b2 <= a3 + a4 + 1;
-        bg <= ag;
-        if (bg) begin
-            samp <= (samp==len-1'b1) ? 0 : samp+1'b1;
-            if (samp==len-1) show <= ~show;
-        end
-    end
+	b1 <= a1 - a2;
+	b2 <= a3 + a4 + 1;
+	if (reset) begin
+		bg <= 0;
+		samp <= 0;
+		show <= 0;
+	end else begin
+		bg <= ag;
+		if (bg) begin
+			samp <= (samp==len-1'b1) ? 0 : samp+1'b1;
+			if (samp==len-1) show <= ~show;
+		end
+	end
 end
 
 wire signed [21:0] c1;
 sat_add #(23,22) finals(clk,b1,b2,c1);
 reg cg=0;
 always @(posedge clk) begin
-    if (reset)
-        cg <= 0;
-    else
-        cg <= bg & show;
-    end
+	if (reset) cg <= 0;
+	else cg <= bg & show;
+end
 
 assign outd = c1[21:2];
 assign outg = cg;
