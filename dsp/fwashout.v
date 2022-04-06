2faa4cd6ac7f4d737eb26f09317babed7b4e0919
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2022-04-05 18:19:21 -0700

    Grab bag of minor maintenance items
    build-tools/merge_json.py: accept (string) hex values for
      base_addr field, but convert them to numeric, and therefore
      always emit them as decimal.
    dsp/circle_buf_serial.v: add plusargs hook "+conveyor_file=%s" for
      sending data stream to file in simulation
      Unfortunately hard-coded for dual signed 20-bit values
    dsp/fwashout.v: comments, including example computation of gain,
      and parameter coding style rearrangement
    projects/cmoc_top/sp605/Makefile: add missing dependencies and CLEAN_DIRS

--- dsp/fwashout.v
+++ dsp/fwashout.v
@@ -3,27 +3,30 @@
 // Name: Washout (DC-reject) filter
 // track input allows freezing DC offset.
 // let N = 2^cut
-// The filter has a Zero at DC [1 + 0j] and 2 poles [0 + 0j], [(N-1)/N + 0j]
-module fwashout(
+// The filter has a z-plane zero at DC [1 + 0j] and 2 poles [0 + 0j], [(N-1)/N + 0j]
+// Evaluating gain at f_s*7/33 using python3:
+// from numpy import exp, pi; cut=4; N=2**cut; p=(N-1)/N
+// z=exp(2j*pi*7/33); gain=(z-1)/z/(z-p); print(abs(gain))
+module fwashout #(
+	parameter a_dw = 16,
+	parameter o_dw = 16,
+	parameter cut = 4
+) (
 	input clk,  // timespec 8.0 ns
-	input rst,
-	input track,
+	input rst,  // active high, resets internal DC offset to zero
+	input track,  // high for normal operation
 	input signed [a_dw-1:0] a_data,  // Raw ADC samples
 	input a_gate, a_trig,
-	output signed [o_dw-1:0] o_data,
+	output signed [o_dw-1:0] o_data,  // Output data
 	output o_gate, o_trig,
 	output time_err
 );
-
-parameter a_dw = 16;
-parameter o_dw = 16;
-parameter cut = 4;
 
 reg signed [a_dw+cut-1:0] dc=0;
 reg signed [a_dw+cut-0:0] sub=0;
 always @(posedge clk) begin
 	if (rst|track) dc <= rst ? 0 : (dc - (dc>>>cut)+a_data);
-	sub <= (a_data<<<cut)-dc+2;
+	sub <= (a_data<<<cut) - dc + 2;
 end
 
 `define SAT(x,old,new) ((~|x[old:new] | &x[old:new]) ? x[new:0] : {x[old],{new{~x[old]}}})
