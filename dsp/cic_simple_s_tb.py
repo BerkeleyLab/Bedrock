dba22fb4b5a8db6ed19f86d7f3ae14a2b459924e
Andrea Bellandi <andrea.bellandi@desy.de>
2022-05-27 13:56:07 +0200

    fixed flake8 on cic_simple_s_tb.py

--- dsp/cic_simple_s_tb.py
+++ dsp/cic_simple_s_tb.py
@@ -10,7 +10,7 @@
 '''
 
 import numpy as np
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
