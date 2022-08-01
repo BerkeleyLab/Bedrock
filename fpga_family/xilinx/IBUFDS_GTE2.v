32bbe3d0e3b67c49629a2a0af454ff89ff987932
Lucas Russo <lerwys@gmail.com>
2022-08-01 15:34:20 -0700

    fpga_family/*/IBUFDS_GTE2.v: add ODIV2 output

--- fpga_family/xilinx/IBUFDS_GTE2.v
+++ fpga_family/xilinx/IBUFDS_GTE2.v
@@ -1,7 +1,11 @@
 `timescale 1ns / 1ns
 
 (* ivl_synthesis_cell *)
-module IBUFDS_GTE2( output O, input I, input IB, input CEB);
+module IBUFDS_GTE2( output O, output ODIV2, input I, input IB, input CEB);
 	parameter DIFF_TERM = "FALSE";
 	buf B1(O, I);
+
+	reg x=0;
+	always @(posedge I) x<=~x;
+	buf B2(ODIV2, x);
 endmodule
