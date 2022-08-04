ba62347c2061b580ada45bce5fe40916b83492e7
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2022-08-04 08:17:37 -0700

    projects/comms_top/qf2_070_3_setup.sh: fix

--- projects/comms_top/qf2_070_3_setup.sh
+++ projects/comms_top/qf2_070_3_setup.sh
@@ -1,3 +1,6 @@
+set -e
 git clone --depth 1 --branch 0.7.0+3 https://gitlab.com/qf2-pre/users.git qf2_users
 (cd qf2_users/qf2_python/QF2_pre/ &&
-ln -s dev_runtime.py v_c268ccebd9ebe93221f75afdb9c67eb28973662dda472bb81d43d81d0731899a.py)
+# when was this symbolic link needed?
+# ln -s dev_runtime.py v_c268ccebd9ebe93221f75afdb9c67eb28973662dda472bb81d43d81d0731899a.py)
+ls -l dev_runtime.py v_c268ccebd9ebe93221f75afdb9c67eb28973662dda472bb81d43d81d0731899a.py || true)
