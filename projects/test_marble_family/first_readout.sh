3458cfbedcff4082ec2c4ce3a15c0b33d16a5977
sdmurthy <sdmurthy@lbl.gov>
2022-12-21 16:40:27 -0800

    Add first_readout.sh to read device status for Marble

---
+++ projects/test_marble_family/first_readout.sh
@@ -0,0 +1,11 @@
+IP=$1
+echo "first_readout $IP"
+set -e
+ping -c 2 $IP
+export PYTHONPATH=../common:../../badger/tests
+python3 -m testcase --ip $IP --stop --trx --si570
+python3 -m peek_mailbox leep://$IP:803
+tt=$(mktemp quick_XXXXXX)
+python3 -m spi_test --ip $IP --udp 804 --otp --pages=1 --dump $tt
+hexdump $tt | head -n 2
+rm $tt
