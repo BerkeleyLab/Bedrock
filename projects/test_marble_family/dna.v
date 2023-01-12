f9ff78f5b0f24c5b95a8d1f2e3ab55835636d874
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2023-01-11 17:06:03 -0800

    dna.v: fuss with style
    Make it look less like VHDL
    No anticipated change in functionality

--- projects/test_marble_family/dna.v
+++ projects/test_marble_family/dna.v
@@ -15,18 +15,14 @@ module dna (
   );
 
   reg [63:0] r_dna;
-  reg [6:0] bcnt;
-  reg shift, r_done, r_start_0, r_start_1;
-  wire read = (bcnt == 7'h0) ? 1'b1 : 1'b0;
-  wire start_re = (r_start_0 == 1'b1) && (r_start_1 == 1'b0) ? 1'b1 : 1'b0;
-  wire din, dout;
+  reg shift=0;
+  wire read, din, dout;
   wire dclk = clk;    // Alias to modify in case clk is >100MHz
-  assign done = r_done;
 
 `ifdef SIMULATE
   assign dna_msb = r_dna[63:32];
   assign dna_lsb = r_dna[31:0];
-  //reg [63:0] r_dna = 64'h4b696e7465782d37;
+  // reg [63:0] r_dna = 64'h4b696e7465782d37;
   assign din = dout;
   reg [63:0] r_dna_int = 64'hb4b73a32bc169ba5;
   reg [63:0] r_dout;
@@ -49,49 +45,47 @@ module dna (
 `else
   assign dna_msb = {7'h00, r_dna[63:39]};
   assign dna_lsb = r_dna[38:7];
-  assign din = 1'b0;      // Pedantically included
+  assign din = 0;      // Pedantically included
   DNA_PORT #(
     .SIM_DNA_VALUE(57'h000006789998212)  // Specifies a sample 57-bit DNA value for simulation
   ) DNA_PORT_inst (
     .DOUT(dout),   // 1-bit output: DNA output data.
-    .CLK(dclk),     // 1-bit input: Clock input.
+    .CLK(dclk),    // 1-bit input: Clock input.
     .DIN(din),     // 1-bit input: User data input pin.
     .READ(read),   // 1-bit input: Active high load DNA, active low read input.
     .SHIFT(shift)  // 1-bit input: Active high shift enable input.
   );
 `endif
 
-  initial begin
-    r_done = 1'b1;
-    shift = 1'b0;
-    bcnt = 7'h0;
-    r_start_0 = 1'b0;
-    r_start_1 = 1'b0;
-  end
+  reg [6:0] bcnt=0;
+  assign read = bcnt == 7'h0;
+  reg r_done=1, r_start_0=0, r_start_1=0;
+  assign done = r_done;
+  wire start_re = r_start_0 && ~r_start_1;
 
   always @(posedge dclk) begin
     if (rst) begin
-      r_done <= 1'b1;
+      r_done <= 1;
       bcnt <= 7'h0;
-      r_start_0 <= 1'b0;
-      r_start_1 <= 1'b0;
+      r_start_0 <= 0;
+      r_start_1 <= 0;
       r_dna <= 64'h0;
     end else begin
       r_start_1 <= r_start_0;
       r_start_0 <= start;
       if (r_done) begin
-        shift <= 1'b0;
-        if (start_re == 1'b1) begin
-          r_done <= 1'b0;
+        shift <= 0;
+        if (start_re) begin
+          r_done <= 0;
         end
       end else begin  // if ~r_done
-        shift <= 1'b1;
+        shift <= 1;
         r_dna <= {r_dna[62:0], dout};
         if (bcnt == 7'd64) begin
-          r_done <= 1'b1;
+          r_done <= 1;
           bcnt <= 7'h0;
         end else begin
-          r_done <= 1'b0;
+          r_done <= 0;
           bcnt <= bcnt + 1;
         end
       end
