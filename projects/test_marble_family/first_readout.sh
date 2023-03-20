c35b958ac02dca03358a67cf413afa5b2501aa75
Shreeharshini Murthy <sdmurthy@lbl.gov>
2023-03-20 11:58:38 -0700

    Test marble family argument refactoring

--- projects/test_marble_family/first_readout.sh
+++ projects/test_marble_family/first_readout.sh
@@ -3,7 +3,7 @@ echo "first_readout $IP"
 set -e
 ping -c 2 $IP
 export PYTHONPATH=../common:../../badger/tests
-python3 -m testcase --ip $IP --stop --trx --si570
+python3 -m testcase -a $IP -p 803 --stop --trx --si570
 python3 -m peek_mailbox leep://$IP:803
 tt=$(mktemp quick_XXXXXX)
 python3 -m spi_test --ip $IP --udp 804 --otp --pages=1 --dump $tt
