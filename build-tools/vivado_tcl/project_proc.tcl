d9684961e90d14032d60a72245ff9502caa46e9a
Lucas Russo <lerwys@gmail.com>
2023-03-28 16:21:17 -0700

    build-tools: fix running vivado to post-route steps if enabled
    Previously the last step was always "route", but
    there is a possibility that the user enabled the
    "post-route physical optimization" step, in which
    case this would not be run and the timing report
    (or the implemented design) would not exist.

--- build-tools/vivado_tcl/project_proc.tcl
+++ build-tools/vivado_tcl/project_proc.tcl
@@ -221,7 +221,12 @@ proc project_run {verilog_defines} {
     wait_on_run synth_1
 
     #launch_runs impl_1 -to_step write_bitstream
-    launch_runs impl_1 -to_step route_design
+    set post_route_phys_opt_design [get_property steps.post_route_phys_opt_design.is_enabled [get_runs impl_1]]
+    if {$post_route_phys_opt_design} {
+        launch_runs impl_1 -to_step "phys_opt_design (Post-Route)"
+    } else {
+        launch_runs impl_1 -to_step route_design
+    }
     wait_on_run impl_1
     open_run impl_1
 }
