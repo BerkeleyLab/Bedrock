793a53a0d9ae26aecb39d9dad90c512abbe14daa
Keith Penney <kpenney@lbl.gov>
2023-07-19 13:48:18 -0700

    Changing match-case to older python if-elif syntax to make old flake8 happy

--- peripheral_drivers/i2cbridge/decode.py
+++ peripheral_drivers/i2cbridge/decode.py
@@ -39,85 +39,84 @@ def decode(argv):
         n_code = cmd_byte & 0x1f
 
         report_file += f"{pa:03x}: {cmd_byte:02x} : [{op_code:03b} {n_code:05b}] -> "
-        match op_code:
-            case 0:  # special
-                if n_code == 0:
-                    report_file += "STOP (sleep)\n"
-                elif n_code == 2:
-                    report_file += "Result buffer flip\n"
-                elif n_code == 3:
-                    report_file += "Trigger logic analyzer\n"
-                elif n_code >= 16:
-                    cfg = n_code & 0xf
-                    report_file += f"Set hw_config = 0b{cfg:04b}\n"
+        if op_code == 0:  # special
+            if n_code == 0:
+                report_file += "STOP (sleep)\n"
+            elif n_code == 2:
+                report_file += "Result buffer flip\n"
+            elif n_code == 3:
+                report_file += "Trigger logic analyzer\n"
+            elif n_code >= 16:
+                cfg = n_code & 0xf
+                report_file += f"Set hw_config = 0b{cfg:04b}\n"
 
-            case 1:  # read
-                dlen = n_code-1
-                devaddr = int(next(cmd_table)[:2], 16)
-                pa += 1
-                s, inc = platform_read(devaddr, dlen, cmd_table)
-                if s is not None:
-                    report_file += s + '\n'
-                    pa += inc
-                else:
-                    report_file += f"Read  - dev_addr: 0x{devaddr:02x} - data number: {dlen}\n"
+        elif op_code == 1:  # read
+            dlen = n_code-1
+            devaddr = int(next(cmd_table)[:2], 16)
+            pa += 1
+            s, inc = platform_read(devaddr, dlen, cmd_table)
+            if s is not None:
+                report_file += s + '\n'
+                pa += inc
+            else:
+                report_file += f"Read  - dev_addr: 0x{devaddr:02x} - data number: {dlen}\n"
 
-            case 2:  # write
-                # n_code = 1 + addr_bytes + len(data)
-                nbytes = n_code-1
-                devaddr = int(next(cmd_table)[:2], 16)
-                pa += 1
-                s, inc = platform_write(devaddr, nbytes, cmd_table)
-                if s is not None:
-                    report_file += s + '\n'
-                    pa += inc
-                else:
-                    report_file += f"Write - dev_addr: 0x{devaddr:02x} - mem_addr+data:"
-                    for j in range(nbytes):
-                        data = int(next(cmd_table)[:2], 16)
-                        pa += 1
-                        report_file += f" 0x{data:02x}"
-                    report_file += '\n'
-
-            case 3:  # write followed by repeated start
-                addr_bytes = n_code-1
-                dlen = n_code-2
-                devaddr = int(next(cmd_table)[:2], 16)
-                memaddr = int(next(cmd_table)[:2], 16)
-                pa += 2
-                if addr_bytes == 2:
-                    memaddr = (memaddr << 8) + int(next(cmd_table)[:2], 16)
+        elif op_code == 2:  # write
+            # n_code = 1 + addr_bytes + len(data)
+            nbytes = n_code-1
+            devaddr = int(next(cmd_table)[:2], 16)
+            pa += 1
+            s, inc = platform_write(devaddr, nbytes, cmd_table)
+            if s is not None:
+                report_file += s + '\n'
+                pa += inc
+            else:
+                report_file += f"Write - dev_addr: 0x{devaddr:02x} - mem_addr+data:"
+                for j in range(nbytes):
+                    data = int(next(cmd_table)[:2], 16)
                     pa += 1
-                s, inc = platform_write_rpt(devaddr, memaddr, dlen, cmd_table)
-                if s is not None:
-                    report_file += s + '\n'
-                    pa += inc
-                else:
-                    report_file += f"Write - dev_addr: 0x{devaddr:02x} - mem_addr:" + \
-                        f" 0x{memaddr:04x} - START - data:"
-                    for j in range(n_code-2):
-                        data = int(next(cmd_table)[:2], 16)
-                        pa += 1
-                        report_file += f" 0x{data:02x}"
-                    report_file += '\n'
+                    report_file += f" 0x{data:02x}"
+                report_file += '\n'
 
-            case 4:  # pause (ticks are 8 bit times)
-                if n_code == 0:
-                    report_file += "pad\n"
-                else:
-                    report_file += f"Short pause of {n_code} cycles\n"
+        elif op_code == 3:  # write followed by repeated start
+            addr_bytes = n_code-1
+            dlen = n_code-2
+            devaddr = int(next(cmd_table)[:2], 16)
+            memaddr = int(next(cmd_table)[:2], 16)
+            pa += 2
+            if addr_bytes == 2:
+                memaddr = (memaddr << 8) + int(next(cmd_table)[:2], 16)
+                pa += 1
+            s, inc = platform_write_rpt(devaddr, memaddr, dlen, cmd_table)
+            if s is not None:
+                report_file += s + '\n'
+                pa += inc
+            else:
+                report_file += f"Write - dev_addr: 0x{devaddr:02x} - mem_addr:" + \
+                    f" 0x{memaddr:04x} - START - data:"
+                for j in range(n_code-2):
+                    data = int(next(cmd_table)[:2], 16)
+                    pa += 1
+                    report_file += f" 0x{data:02x}"
+                report_file += '\n'
 
-            case 5:  # pause (ticks are 256 bit times)
-                report_file += f"Long pause of {n_code*32} cycles\n"
+        elif op_code == 4:  # pause (ticks are 8 bit times)
+            if n_code == 0:
+                report_file += "pad\n"
+            else:
+                report_file += f"Short pause of {n_code} cycles\n"
 
-            case 6:  # jump
-                jump_addr = n_code*32
-                report_file += f"Jump to address 0x{jump_addr:03x}\n"
+        elif op_code == 5:  # pause (ticks are 256 bit times)
+            report_file += f"Long pause of {n_code*32} cycles\n"
 
-            case 7:  # set result address
-                result_address = 0x800 + n_code*32
-                report_file += f"Set result address to 0x{result_address:03x}" + \
-                    f" (0x{n_code:02x})\n"
+        elif op_code == 6:  # jump
+            jump_addr = n_code*32
+            report_file += f"Jump to address 0x{jump_addr:03x}\n"
+
+        elif op_code == 7:  # set result address
+            result_address = 0x800 + n_code*32
+            report_file += f"Set result address to 0x{result_address:03x}" + \
+                f" (0x{n_code:02x})\n"
         pa += 1
 
     report_file += "---- End of report ----\n"
