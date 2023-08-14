40123768e59f7709ca04f81cea23ee51a6e96e31
Keith Penney <kpenney@lbl.gov>
2023-08-14 15:50:12 -0700

    Exception handling

--- projects/test_marble_family/badger_stat.py
+++ projects/test_marble_family/badger_stat.py
@@ -1,7 +1,9 @@
 import leep
 from time import sleep
 from sys import argv
+import sys
 import numpy as np
+import socket
 prevs = np.array([0]*16)
 
 
@@ -22,11 +24,22 @@ if __name__ == "__main__":
         interval = float(argv[2])
     else:
         interval = 2.0  # seconds
-    dev = leep.open(argv[1])
+    try:
+        dev = leep.open(argv[1])
+    except TimeoutError:
+        print("Socket error")
+        sys.exit(1)
 
     print("Packet badger stats from", argv[1])
     print("   n/a  !crc   arp  !MAC   !IP  other  ICMP  n/a   UDP     1     2     3")
     while True:
-        counts = update(dev)
-        print("".join(["%6d" % counts[ix] for ix in range(16)]))
-        sleep(interval)
+        try:
+            counts = update(dev)
+            print("".join(["%6d" % counts[ix] for ix in range(16)]))
+            sleep(interval)
+        except (KeyboardInterrupt, TimeoutError) as err:
+            if isinstance(err, KeyboardInterrupt):
+                print("\nExiting")
+            else:
+                print("Socket timeout")
+            break
