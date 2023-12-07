8005d8bac18c07d45baeba190abb4f71a22bbd2a
Alessio Amodio <aamodio@lbl.gov>
2023-12-07 10:52:46 -0800

    marble.tcl:in case of dirty commit the bitfile filename contains "dirty"

--- projects/test_marble_family/marble.tcl
+++ projects/test_marble_family/marble.tcl
@@ -36,7 +36,7 @@ set_property top "marble_top" [current_fileset]
 set_property verilog_define [list "CHIP_FAMILY_7SERIES"] [current_fileset]
 
 # Get git commit ID
-set gitid [exec git rev-parse --short=8 --verify HEAD]
+set gitid [exec git describe --always --abbrev=8 --dirty]
 set gitid_v 32'h$gitid
 set new_defs [list "GIT_32BIT_ID=$gitid_v" "REVC_1W"]
 
