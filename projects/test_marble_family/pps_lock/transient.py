fa08bcdc3c83d94edfffd54a67fd3145871183ae
Larry Doolittle <LRDoolittle@lbl.gov>
2024-01-26 16:38:24 -0800

    transient.py: churn
    Can now plot a time-domain result

--- projects/test_marble_family/pps_lock/transient.py
+++ projects/test_marble_family/pps_lock/transient.py
@@ -2,18 +2,16 @@
 # digital time detect between PPS input and divider, DAC output to VCXO
 import numpy as np
 from matplotlib import pyplot
+from scipy import signal
 fir_enable = True
 
 
-def getTransient(clk_freq=125.0e6, dac_dw=16, vcxo_ppm=14, Kp=-8, Ki=-1):
+def getTransfer(z, clk_freq=125.0e6, dac_dw=16, vcxo_ppm=14, Kp=-8, Ki=-1):
     # 125 MHz * 1 second * 14 ppm / 65535 counts
     A = clk_freq*vcxo_ppm*(1.0e-6)/(1 << dac_dw)  # phase counts / DAC counts
     # Kp = -8  # DAC counts / phase counts
     # Ki = -1  # DAC counts / phase counts / cycle
 
-    f = (np.arange(1, 499)/500.0) * 0.5  # Hz
-    T = 1  # second, sample rate
-    z = np.exp(2*np.pi*f*T*1j)
     zi = 1/z
 
     g1 = A * zi / (1-zi)        # plant
@@ -24,28 +22,36 @@ def getTransient(clk_freq=125.0e6, dac_dw=16, vcxo_ppm=14, Kp=-8, Ki=-1):
     g_df = g1 / (1 - g1*g2)  # phase response to VCXO noise
     g_dp = 1 / (1 - g1*g2)   # phase response to phase measurement noise
 
-    # Can also process this analytically.
-    # Without the FIR filter, use maxima to find the denominator:
+    # Processing that equation analytically can convert those expressions into the
+    # canonical ratio-of-z-polynomial form.  That's helpful for checking stability,
+    # and for using scipy.signal.lfilter().
+    # Both the above expressions have the same poles, and
+    # zeros of the denominator of 1/(1-g1*g2) represent those poles.
+    # Here I use maxima.
+    #
+    # Without the FIR filter:
     #   g1: A * zi / (1-zi);
     #   g2: Kp + Ki * zi / (1-zi);
-    #   ratsimp((1-zi)^2*(1-g1*g2));
-    # result:
+    #   denom(ratsimp((1/(1-g1*g2))));
+    # hand-formatted result:
     #   1 + z^{-1}*(-2-Kp*A) + z^{-2}*(1+Kp*A-Ki*A)
     # Repeat with the FIR filter:
     #   g1: A * zi / (1-zi);
     #   g2: (Kp + Ki * zi / (1-zi))*(1+zi)/2;
-    #   ratsimp((1-zi)^2*(1-g1*g2));
-    # result:
-    #   (2 + z^{-1}*(-4-Kp*A) + z^{-2}*(2-Ki*A) + z^{-3}*(Kp*A-Ki*A))/2
-    # Express that denominator as a polynomial in z:
+    #   denom(ratsimp(1/(1-g1*g2)));
+    # hand-formatted result:
+    #   2 + z^{-1}*(-4-Kp*A) + z^{-2}*(2-Ki*A) + z^{-3}*(Kp*A-Ki*A)
+    # Express those denominator as polynomials in z:
     if fir_enable:
         dpoly = [1, -2-0.5*Kp*A, 1-0.5*Ki*A, 0.5*A*(Kp-Ki)]
     else:
         dpoly = [1, (-2-Kp*A), (1+Kp*A-Ki*A)]
     dpoly = np.array(dpoly)
 
+    # Print the result, find the roots (poles) numerically, and see if
+    # any of those roots are outside the unit circle, representing instability.
     np.set_printoptions(precision=4, floatmode="fixed", suppress=False)
-    print("Poly", dpoly)
+    print("Denominator coefficients", dpoly)
     rr = np.roots(dpoly)
     print("Roots", rr)
     print("Root mags", abs(rr))
@@ -53,24 +59,45 @@ def getTransient(clk_freq=125.0e6, dac_dw=16, vcxo_ppm=14, Kp=-8, Ki=-1):
         print("Stable!  :-)")
     else:
         print("Unstable!  :-(")
+    npoly = np.array([1, -2, 1])
 
-    # then cross-check numerically
-    den = np.polyval(dpoly, zi)
-    g_dp2 = (1-zi)**2 / den
-    return (f, g_df, g_dp, g_dp2, A)
+    return (g_df, g_dp, A, npoly, dpoly)
 
 
-def plotTransient(f, g_df, g_dp, g_dp2, A):
+def plotTransfer(f, g_df, g_dp, g_dp2, A):
     pyplot.plot(f, abs(g_df)/A, label='df')
     pyplot.plot(f, abs(g_dp), label='dp')
     pyplot.plot(f, abs(g_dp2), label='dp2')
     pyplot.legend(frameon=False)
     pyplot.xlabel("Frequency (Hz)")
-    pyplot.ylabel("?? (??)")
+    pyplot.ylabel("Gain")
     pyplot.show()
     return
 
 
+def genTransferPlot():
+    f = (np.arange(1, 499)/500.0) * 0.5  # Hz
+    T = 1  # second, sample rate
+    z = np.exp(2*np.pi*f*T*1j)
+    zi = 1/z
+    g_df, g_dp, A, npoly, dpoly = getTransfer(z)
+    #
+    # Quick evaluation of the canonical polynomial form.
+    # If our symbolic math was done correctly,
+    # it (g_dp2) should match the direct numerical calculation (g_dp).
+    g_dp2 = np.polyval(npoly, zi) / np.polyval(dpoly, zi)
+    plotTransfer(f, g_df, g_dp, g_dp2, A)
+    return (npoly, dpoly)
+
+
+def genTimeDomain(polys):
+    npoly, dpoly = polys
+    y = signal.lfilter(npoly, dpoly, 100*[1])
+    pyplot.plot(y)
+    pyplot.xlabel("time (s)")
+    pyplot.show()
+
+
 if __name__ == "__main__":
-    data = getTransient()
-    plotTransient(*data)
+    polys = genTransferPlot()
+    genTimeDomain(polys)
