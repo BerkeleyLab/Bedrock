6fdd4710591965e9697bb2ef760bb73b0435ab49
Qiang Du <QDu@lbl.gov>
2024-08-11 15:28:12 -0700

    localbus/gen_regmap.py: Set default address width to 24

--- localbus/gen_regmap.py
+++ localbus/gen_regmap.py
@@ -14,7 +14,7 @@ header = """
 """
 
 
-def gen_addrmap(regmap):
+def gen_addrmap(regmap, cut_off_aw=5):
     """
     Collect all addresses as keys to the addrmap dict. Values are the names.
     """
@@ -29,13 +29,13 @@ def gen_addrmap(regmap):
             if addri >= (0x01000000 >> 2):
                 raise ValueError("Localbus address outside valid range!")
 
-            if 0 < aw <= 6:  # cutoff array generation if length > 32.
+            if 0 < aw <= cut_off_aw:  # cutoff array generation
                 for ix in range(1 << aw):
                     addrmap[addri + ix] = key + '_' + str(ix)
             elif aw == 0:
                 addrmap[addri] = key
             else:
-                print("Large array ignored (len>32), key: {}".format(key))
+                print(f"Large array ignored (len>{2**cut_off_aw}), key: {key}")
     return addrmap
 
 
@@ -46,17 +46,17 @@ def write_addrmap(addrmap, ifname, ofname, adrw=18):
 
     # header = ofname.replace('.', '_').upper()
     for addr in sorted(addrmap.keys()):
-        hf += "localparam [{:d}:0] {:32s} = 20'h{:05x};\n".format(
-                adrw-1, addrmap[addr].upper(), addr)
+        hf += "localparam [{:d}:0] {:32s} = {:d}'h{:05x};\n".format(
+                adrw-1, addrmap[addr].upper(), adrw, addr)
 
     with open(ofname, "w") as f:
         f.write(hf)
 
 
-def main(json_fname, dest_fname, adrw=18):
+def main(json_fname, dest_fname, adrw=18, cut_off_aw=5):
     with open(json_fname) as s:
         regmap = json.load(s)
-        addrmap = gen_addrmap(regmap)
+        addrmap = gen_addrmap(regmap, cut_off_aw=cut_off_aw)
         write_addrmap(addrmap, json_fname, dest_fname, adrw)
 
 
@@ -64,7 +64,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', help='input json file')
     parser.add_argument('-o', '--output', help='output verilog header file')
-    parser.add_argument('-w', '--adrw', help='lb address width', default=18)
+    parser.add_argument('-w', '--adrw',
+                        type=int, help='lb address width', default=24)
+    parser.add_argument(
+        '-c', '--cut-off-aw', type=int, default=5,
+        help="cut off array generation if address width is greater")
     args = parser.parse_args()
 
-    main(args.input, args.output, args.adrw)
+    main(args.input, args.output, args.adrw, args.cut_off_aw)
