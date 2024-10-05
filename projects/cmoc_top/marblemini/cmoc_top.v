be94c9ab8b6faa1890e85b9001eaa01dda91102b
Larry Doolittle <LRDoolittle@lbl.gov>
2024-10-05 14:28:12 -0700

    marblemini/cmoc_top.v: reduce CRITICAL WARNING count
    by aligning a bit better wtih marble_top.v

--- projects/cmoc_top/marblemini/cmoc_top.v
+++ projects/cmoc_top/marblemini/cmoc_top.v
@@ -50,7 +50,19 @@ module cmoc_top(
 
 	output       VCXO_EN,
 
-	output [7:0] LED
+	output [7:0] LED,
+
+	// J15 TMDS 0, 1, 2, CLK
+	output [3:0] TMDS_P,
+	output [3:0] TMDS_N,
+
+	// Directly attached LEDs
+	output LD16,
+	output LD17,
+
+	// Physical Pmod
+	// Pmod1: use LED above with Digilent mapping
+	input [7:0] Pmod2
 );
 
 assign VCXO_EN = 1;
@@ -60,6 +72,9 @@ IBUFDS_GTE2 passi_125(.I(GTPREFCLK_P), .IB(GTPREFCLK_N), .CEB(1'b0), .O(gtpclk0)
 // Vivado fails, with egregiously useless error messages,
 // if you don't put this BUFG in the chain to the MMCM.
 BUFG passg_125(.I(gtpclk0), .O(gtpclk));
+
+wire si570;
+assign si570 = 0;
 
 parameter in_phase_tx_clk = 1;
 // Standardized interface, hardware-dependent implementation
@@ -161,12 +176,11 @@ marble_base #(.USE_I2CBRIDGE(1)) base(
 	.vgmii_tx_en(vgmii_tx_en), .vgmii_tx_er(vgmii_tx_er),
 	.vgmii_rx_clk(vgmii_rx_clk), .vgmii_rxd(vgmii_rxd),
 	.vgmii_rx_dv(vgmii_rx_dv), .vgmii_rx_er(vgmii_rx_er),
-	.phy_rstn(PHY_RSTN), .clk_locked(clk_locked),
+	.phy_rstn(PHY_RSTN), .clk_locked(clk_locked), .si570(si570),
 	.boot_clk(BOOT_CCLK), .boot_cs(BOOT_CS_B),
 	.boot_mosi(BOOT_MOSI), .boot_miso(BOOT_MISO),
 	.cfg_d02(CFG_D02), .mmc_int(MMC_INT), .ZEST_PWR_EN(ZEST_PWR_EN),
 	.aux_clk(SYSCLK_P), .clk62(clk62), .cfg_clk(cfg_clk),
-	.GPS(4'b0),
 	.SCLK(SCLK), .CSB(CSB), .MOSI(MOSI), .MISO(MISO),
 	.FPGA_RxD(FPGA_RxD), .FPGA_TxD(FPGA_TxD),
 	.twi_scl({dum_scl, old_scl1, old_scl2, TWI_SCL}),
@@ -185,12 +199,19 @@ marble_base #(.USE_I2CBRIDGE(1)) base(
 		FMC1_CK_P, FMC1_CK_N, FMC1_LA_P, FMC1_LA_N}),
 	.WR_DAC_SCLK(WR_DAC_SCLK), .WR_DAC_DIN(WR_DAC_DIN),
 	.WR_DAC1_SYNC(WR_DAC1_SYNC), .WR_DAC2_SYNC(WR_DAC2_SYNC),
-	.LED(LED)
+	.LED(LED), .GPS(Pmod2[3:0])
 );
 // TODO: Removing the SPI flash for now
 //defparam base.rtefi.p4_client.engine.seven = 1;
 
 parameter BUF_AW=13;
+
+assign LD16 = LED[0];
+assign LD17 = LED[1];
+
+// test_marble_family instantiates a "Silly little test pattern generator" for these pins
+wire [3:0] tmds_q = 4'b0000;
+OBUFDS obuf[0:3] (.I(tmds_q), .O(TMDS_P), .OB(TMDS_N));
 
 // These are meaningful LEDs, one could use them
 wire [2:0] D4rgb;
