e0e94acf86e930822e029c4a8da6f45e1a2b14bd
Larry Doolittle <LRDoolittle@lbl.gov>
2024-10-16 19:58:59 -0700

    Tiny build system tweaks
    selftest.sh: print nmigen version
    localbus/Makefile: remove tgen_tb in a make clean

--- selftest.sh
+++ selftest.sh
@@ -39,7 +39,7 @@ yosys -V
 echo 'puts "tclsh [info patchlevel]"' | tclsh
 flake8 --version
 if [ "$1" = "more" ]; then
-python3 -c 'import nmigen; print("nmigen found")'
+python3 -c 'import nmigen; print("nmigen found", nmigen.__version__)'
 riscv64-unknown-elf-gcc --version
 fi
 
