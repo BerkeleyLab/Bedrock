679339f6607acb2d85c2d15f21da343383a28c0b
Larry Doolittle <LRDoolittle@lbl.gov>
2024-11-13 13:34:40 -0800

    Maybe improve reliability of badger_ac701_run CI job
    Specify USB serial number for bitfile load

--- badger/tests/teststand_ac701.sh
+++ badger/tests/teststand_ac701.sh
@@ -4,7 +4,7 @@
 # Tested useful for the CI test stand (mohs) at LBNL.
 # Can also act as a template for other use cases.
 set -e
-xc3sprog -c jtaghs1_fast ac701_rgmii_vtest.bit
+xc3sprog -c jtaghs1_fast $SERIAL_NUM_OPT ac701_rgmii_vtest.bit
 echo "So far so good"
 sleep 8
 echo "Hope links are up"
