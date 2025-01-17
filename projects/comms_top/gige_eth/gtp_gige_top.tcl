5ef7764c523139bd9b05721e1b562928d47e8190
Larry Doolittle <lrdoolittle@lbl.gov>
2025-01-16 18:49:31 -0800

    Add structures to implement GigE using 16-bit GTP at 125 MHz

--- projects/comms_top/gige_eth/gtp_gige_top.tcl
+++ projects/comms_top/gige_eth/gtp_gige_top.tcl
@@ -17,7 +17,10 @@ source $MGT_CONFIG_DIR/mgt_gen.tcl
 set quad 0
 set pll0_refclk "REFCLK0"
 set pll1_refclk "REFCLK0"
+# Stupid but working with 62.5 MHz clk:
 add_gtcommon $MGT_CONFIG_DIR/gtp_common_1_25.tcl $quad $pll0_refclk $pll1_refclk
+# for DOUBLEBIT experiments:
+# add_gtcommon $MGT_CONFIG_DIR/gtp_common_2_50.tcl $quad $pll0_refclk $pll1_refclk
 
 # proc add_gt_protocol {gt_type config_file quad_num gt_num en8b10b pll_type}
 
@@ -28,7 +31,10 @@ set gt 0
 set en8b10b 0
 set endrp 0
 set pll_type "PLL0"
+# Stupid but working with 62.5 MHz clk:
 add_gt_protocol $gt_type $MGT_CONFIG_DIR/gtp_ethernet.tcl $quad $gt $en8b10b $endrp $pll_type
+# for DOUBLEBIT experiments:
+# add_gt_protocol $gt_type $MGT_CONFIG_DIR/gtp_ethernet_2_50.tcl $quad $gt $en8b10b $endrp $pll_type
 
 # proc add_aux_ip {ipname config_file module_name}
 add_aux_ip clk_wiz $MGT_CONFIG_DIR/mgt_eth_clk.tcl mgt_eth_mmcm
