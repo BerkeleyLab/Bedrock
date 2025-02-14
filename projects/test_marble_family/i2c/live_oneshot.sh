7198cecc5e396f83597be72e0b2f54931909ef06
Keith Penney <kpenney@lbl.gov>
2025-02-14 14:41:17 -0800

    Applying updates from LRD. shellcheck'd live_oneshot.sh. Includes i2c in selftest

--- projects/test_marble_family/i2c/live_oneshot.sh
+++ projects/test_marble_family/i2c/live_oneshot.sh
@@ -1,13 +1,15 @@
 # Do a pair of write/read cycles on live hardware
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
   echo "Please provide an IP address"
   exit 1
 fi
-IP=$1
+IP="$1"
+LEEP="leep://$IP:803"
+PYTHON=python3
 set -e
-export PYTHONPATH=../../../peripheral_drivers/i2cbridge:../../common:$PYTHONPATH
-python3 -m oneshot leep://$IP:803 U39.3=0x80
-test "$(python3 -m oneshot leep://$IP:803 U39.3)" = "U39_3: 0x80"
-python3 -m oneshot leep://$IP:803 U39.3=0x88
-test "$(python3 -m oneshot leep://$IP:803 U39.3)" = "U39_3: 0x88"
+export PYTHONPATH="../../../peripheral_drivers/i2cbridge:../../common:$PYTHONPATH"
+$PYTHON -m oneshot "$LEEP" U39.3=0x80
+test "$($PYTHON -m oneshot "$LEEP" U39.3)" = "U39_3: 0x80"
+$PYTHON -m oneshot "$LEEP" U39.3=0x88
+test "$($PYTHON -m oneshot "$LEEP" U39.3)" = "U39_3: 0x88"
 echo "PASS"
