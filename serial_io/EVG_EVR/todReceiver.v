7f74095eca0801ae1b0dcffa7e5dd455487a96f2
Lucas Russo <lerwys@gmail.com>
2025-04-10 11:04:32 -0700

    EVG_EVR: add more accurate time-of-day receiver
    This module measures the clock rate constantly to
    get a more accurate increment for the 32-bit nanoseconds
    part. This means that the nanoseconds register is not
    the old "number of ticks since PPS" anymore, but a fraction
    of a second.
    
    To convert from fraction to nanoseconds, you have to divide
    the fraction register with the factor 2^32/1e9 (=4.294967296).

---
+++ serial_io/EVG_EVR/todReceiver.v
@@ -0,0 +1,283 @@
+//  Time of day receiver module
+//
+//  It receives time stamp control events to set the
+//  time of day from the GPS locked NTP server (usually
+//  an upstream event generator).
+//
+//  This design was heavily based on W. Eric Norum module
+//  to count seconds' fraction by measuring the clock rate
+
+module todReceiver #(
+    parameter       NOMINAL_CLK_RATE      = 125_000_000,
+    parameter       TIMESTAMP_WIDTH       = 64,
+    parameter [7:0] EVCODE_SHIFT_ZERO     = 8'h70,
+    parameter [7:0] EVCODE_SHIFT_ONE      = 8'h71,
+    parameter [7:0] EVCODE_SECONDS_MARKER = 8'h7D,
+    parameter       STATUS_COUNTER_WIDTH  = 10
+) (
+    input                                    clk,
+    input                                    rst,
+
+    input wire                         [7:0] evCode,
+    input wire                               evCodeValid,
+
+    output reg    [STATUS_COUNTER_WIDTH-1:0] tooManyBitsCounter = 0,
+    output reg    [STATUS_COUNTER_WIDTH-1:0] tooFewBitsCounter = 0,
+    output reg    [STATUS_COUNTER_WIDTH-1:0] outOfSeqCounter = 0,
+    output wire   [TIMESTAMP_WIDTH-1:0]      timestamp,
+    output wire                              timestampValid,
+    output wire   [TIMESTAMP_WIDTH-1:0]      timestampHA,
+    output wire                              timestampHAValid
+);
+
+localparam SECONDS_WIDTH = TIMESTAMP_WIDTH/2;
+localparam FRACTION_WIDTH = TIMESTAMP_WIDTH/2;
+reg [SECONDS_WIDTH-1:0] seconds = 0, expectSeconds = 0;
+
+localparam TICKS_WIDTH = TIMESTAMP_WIDTH/2;
+reg [TICKS_WIDTH-1:0] ticks = 0;
+
+// High-accuracy timestamp
+assign timestampHA = {seconds, fraction};
+// Old-style timestamp, counting ticks. Kept for compatibility
+assign timestamp = {seconds, ticks};
+
+reg         [SECONDS_WIDTH-1:0] shiftReg;
+reg [$clog2(SECONDS_WIDTH)-1:0] bitsLeft = SECONDS_WIDTH - 1;
+reg enoughBits = 0, tooManyBits = 0;
+
+// Count clocks per second
+localparam PPS_INITIAL_INTERVAL = (NOMINAL_CLK_RATE / 100) * 99;
+localparam PPS_WINDOW_INTERVAL = NOMINAL_CLK_RATE / 50;
+localparam CLK_COUNTER_WIDTH = $clog2(PPS_INITIAL_INTERVAL+PPS_WINDOW_INTERVAL+1);
+
+// Low pass filter clocks per second
+localparam FRACTION_WIDEN = 12;
+localparam FILTER_L2_ALPHA = 4;
+localparam FILTER_ACCUMULATOR_WIDTH = CLK_COUNTER_WIDTH + FILTER_L2_ALPHA;
+reg [FILTER_ACCUMULATOR_WIDTH-1:0] filterAccumulator =
+                                                    NOMINAL_CLK_RATE << FILTER_L2_ALPHA;
+wire [CLK_COUNTER_WIDTH-1:0] filteredClocksPerSecond =
+             filterAccumulator[FILTER_ACCUMULATOR_WIDTH-1-:CLK_COUNTER_WIDTH];
+
+// Validate PPS
+localparam PPS_INITIAL_WIDTH = $clog2(PPS_INITIAL_INTERVAL+1)+1;
+localparam PPS_WINDOW_WIDTH = $clog2(PPS_WINDOW_INTERVAL+1)+1;
+
+// Accumlate fractional seconds
+localparam FRACTION_ACCUMULATOR_WIDTH = 32 + FRACTION_WIDEN;
+localparam FRACTION_INCREMENT_WIDTH = $clog2((1<<30)/(PPS_INITIAL_INTERVAL/4)) +
+                                                                 FRACTION_WIDEN;
+
+wire ppsStrobe = evCodeValid && (evCode == EVCODE_SECONDS_MARKER);
+
+///////////////////////////////////////////////////////////////////////////////
+// Measure clock rate
+///////////////////////////////////////////////////////////////////////////////
+
+reg [2:0] ppsValidCounter = 0;
+wire ppsValid = ppsValidCounter[2];
+reg secondsValid = 0;
+
+reg [CLK_COUNTER_WIDTH-1:0] clockCounter = 0;
+reg [PPS_INITIAL_WIDTH-1:0] ppsInitial = 0;
+reg [PPS_WINDOW_WIDTH-1:0] ppsWindow = 0;
+
+wire ppsInitialDone = ppsInitial[PPS_INITIAL_WIDTH-1];
+wire ppsWindowDone = ppsWindow[PPS_WINDOW_WIDTH-1];
+
+always @(posedge clk) begin
+    if (ppsStrobe) begin
+        clockCounter <= 1;
+        ppsInitial <= PPS_INITIAL_INTERVAL - 1;
+        ppsWindow <= PPS_WINDOW_INTERVAL - 1;
+        if (ppsInitialDone && !ppsWindowDone) begin
+            if (!ppsValid) begin
+                ppsValidCounter <= ppsValidCounter + 1;
+            end
+        end
+        else begin
+            ppsValidCounter <= 0;
+        end
+    end
+    else begin
+        clockCounter <= clockCounter + 1;
+        if (ppsInitialDone) begin
+            if (ppsWindowDone) begin
+                ppsValidCounter <= 0;
+            end
+            else begin
+                ppsWindow <= ppsWindow - 1;
+            end
+        end
+        else begin
+            ppsInitial <= ppsInitial - 1;
+        end
+    end
+end
+
+///////////////////////////////////////////////////////////////////////////////
+// Compute fractional seconds increment
+///////////////////////////////////////////////////////////////////////////////
+
+// Compute fractional second increment from filtered clocks per second
+localparam DIVIDER_BITCOUNT_WIDTH = $clog2(FRACTION_INCREMENT_WIDTH)+1;
+
+reg [DIVIDER_BITCOUNT_WIDTH-1:0] dividerBitsLeft;
+wire dividerDone = dividerBitsLeft[DIVIDER_BITCOUNT_WIDTH-1];
+
+reg [CLK_COUNTER_WIDTH:0] dividend;
+reg [FRACTION_INCREMENT_WIDTH-1:0] quotient;
+reg dividerStart = 0;
+reg [FRACTION_INCREMENT_WIDTH-1:0] fractionIncrement =
+                                   {1'b1, {32+FRACTION_WIDEN{1'b0}}} / NOMINAL_CLK_RATE;
+
+always @(posedge clk) begin
+    // First low-pass filter the clock rate measurement
+    if (ppsStrobe && ppsInitialDone && !ppsWindowDone) begin
+        filterAccumulator <= filterAccumulator -
+                             (filterAccumulator >> FILTER_L2_ALPHA) +
+                             clockCounter;
+        dividerStart <= 1;
+    end
+    else begin
+        dividerStart <= 0;
+    end
+
+    // Then compute the fraction increment:
+    // fractionIncrement = (2^(32 + FILTER_L2_ALPHA)) / filteredClocksPerSecond
+    if (dividerStart) begin
+        dividerBitsLeft <= FRACTION_INCREMENT_WIDTH;
+        dividend <= {1'b1, {CLK_COUNTER_WIDTH-1{1'b0}}};
+    end
+    else if (!dividerDone) begin
+        dividerBitsLeft <= dividerBitsLeft - 1;
+        if (dividend >= filteredClocksPerSecond) begin
+            dividend <= (dividend - filteredClocksPerSecond) << 1;
+            quotient <= (quotient << 1) | 1;
+        end
+        else begin
+            dividend <= dividend << 1;
+            quotient <= (quotient << 1);
+        end
+    end
+    else begin
+        fractionIncrement <= quotient;
+    end
+end
+
+///////////////////////////////////////////////////////////////////////////////
+// Update fraction seconds
+///////////////////////////////////////////////////////////////////////////////
+
+reg [FRACTION_ACCUMULATOR_WIDTH-1:0] fractionAccumulator;
+wire [FRACTION_ACCUMULATOR_WIDTH:0] nextFractionAccumulator =
+                                        fractionAccumulator + fractionIncrement;
+wire fractionOverflow = nextFractionAccumulator[FRACTION_ACCUMULATOR_WIDTH];
+wire [FRACTION_WIDTH-1:0] fraction = fractionAccumulator[FRACTION_ACCUMULATOR_WIDTH-1-:FRACTION_WIDTH];
+
+always @(posedge clk) begin
+    if (rst) begin
+        fractionAccumulator <= 0;
+    end
+    else begin
+        // Update fractional seconds
+        if (ppsStrobe) begin
+            fractionAccumulator <= 0;
+        end
+        else if (fractionOverflow) begin
+            fractionAccumulator <= ~0;
+        end
+        else begin
+            fractionAccumulator <= fractionAccumulator + fractionIncrement;
+        end
+    end
+end
+
+///////////////////////////////////////////////////////////////////////////////
+// Status counters
+///////////////////////////////////////////////////////////////////////////////
+always @(posedge clk) begin
+    if (rst) begin
+        tooFewBitsCounter <= 0;
+        tooFewBitsCounter <= 0;
+    end
+    else begin
+        if (ppsStrobe) begin
+            if (!enoughBits)
+                tooFewBitsCounter <= tooFewBitsCounter + 1;
+
+            if (tooManyBits)
+                tooManyBitsCounter <= tooManyBitsCounter + 1;
+        end
+    end
+end
+
+///////////////////////////////////////////////////////////////////////////////
+// Seconds receiver
+///////////////////////////////////////////////////////////////////////////////
+
+assign timestampHAValid = secondsValid && ppsValid;
+assign timestampValid = secondsValid && ppsValid;
+
+always @(posedge clk) begin
+    if (rst) begin
+        seconds <= 0;
+        ticks <= 0;
+        secondsValid <= 0;
+        enoughBits <= 0;
+        tooManyBits <= 0;
+    end
+    else begin
+        if (ppsStrobe) begin
+            if (enoughBits && !tooManyBits) begin
+                expectSeconds <= shiftReg + 1;
+                if (shiftReg == expectSeconds) begin
+                    seconds <= shiftReg;
+                    secondsValid <= 1;
+                end
+                else begin
+                    outOfSeqCounter <= outOfSeqCounter + 1;
+                    if (secondsValid) begin
+                        seconds <= seconds + 1;
+                    end
+                end
+            end
+            else if (secondsValid) begin
+                seconds <= seconds + 1;
+            end
+
+            ticks <= 0;
+            bitsLeft <= SECONDS_WIDTH - 1;
+            enoughBits <= 0;
+            tooManyBits <= 0;
+        end
+        else begin
+            if (fractionOverflow) begin
+                secondsValid <= 0;
+            end
+
+            if (ticks[TICKS_WIDTH-1] == 0) begin
+                ticks <= ticks + 1;
+            end
+            else begin
+                secondsValid <= 0;
+            end
+        end
+
+        if(evCodeValid &&
+            (evCode == EVCODE_SHIFT_ZERO) || (evCode == EVCODE_SHIFT_ONE)) begin
+            // Shift in another bit of upcoming seconds
+            bitsLeft <= bitsLeft - 1;
+            if (enoughBits)
+                tooManyBits <= 1;
+
+            if (bitsLeft == 0)
+                enoughBits <= 1;
+
+            shiftReg <= {shiftReg[SECONDS_WIDTH-2:0], evCode[0]};
+        end
+    end
+end
+
+endmodule
---
+++ serial_io/EVG_EVR/todReceiver_tb.v
@@ -0,0 +1,219 @@
+// Scale time by a factor of 10000
+
+`timescale 10us / 1ns
+
+module todReceiver_tb #(
+    parameter [7:0] EVCODE_SHIFT_ZERO     = 8'h70,
+    parameter [7:0] EVCODE_SHIFT_ONE      = 8'h71,
+    parameter [7:0] EVCODE_SECONDS_MARKER = 8'h7D,
+    parameter NOMINAL_CLK_RATE = 10_000,
+    parameter TIMESTAMP_WIDTH = 64,
+    parameter TOD_SECONDS_WIDTH = TIMESTAMP_WIDTH / 2,
+    parameter TOD_FRACTION_WIDTH = TIMESTAMP_WIDTH / 2,
+    parameter TOD_TICKS_WIDTH = TIMESTAMP_WIDTH / 2,
+    parameter integer TIMESTAMP_INVALID_ALLOWANCE = 5
+);
+
+reg clk;
+
+integer cc;
+integer errors = 0;
+
+initial begin
+    if ($test$plusargs("vcd")) begin
+        $dumpfile("todReceiver.vcd");
+        $dumpvars(5,todReceiver_tb);
+    end
+
+    clk = 1;
+    for (cc = 0; cc < 100000; cc = cc+1) begin
+        clk = 1; #5;
+        clk = 0; #5;
+    end
+
+	if (errors) begin
+		$display("FAIL");
+		$stop(0);
+	end else begin
+		$display("PASS");
+		$finish(0);
+	end
+end
+
+//////////////////////////////////
+// PPS generation
+//////////////////////////////////
+
+integer tBase = 0;
+reg pps = 0;
+
+always @(posedge clk) begin
+    if (($time - tBase) > 100000) begin
+        if (($time - tBase) > 100010) begin
+            tBase = tBase + 100000;
+        end
+        pps <= ~$time & 1;
+    end
+    else begin
+        pps <= 0;
+    end
+end
+
+localparam PPS_STROBE_DELAY_CHAIN_LENGTH = 3;
+
+reg pps_d = 0;
+reg ppsStrobe = 0;
+reg ppsStrobe_d[0:PPS_STROBE_DELAY_CHAIN_LENGTH-1];
+
+always @(posedge clk) begin
+    pps_d <= pps;
+    ppsStrobe_d[0] <= ppsStrobe;
+
+    ppsStrobe <= 0;
+    if (pps && !pps_d) begin
+        ppsStrobe <= 1;
+    end
+end
+
+genvar i;
+generate
+    for (i = 0; i < PPS_STROBE_DELAY_CHAIN_LENGTH-1; i = i + 1) begin
+        always @(posedge clk) begin
+            ppsStrobe_d[i+1] <= ppsStrobe_d[i];
+        end
+    end
+endgenerate
+
+// dump ppsStrobe_d
+integer j = 0;
+initial begin
+    for (j = 0; j < PPS_STROBE_DELAY_CHAIN_LENGTH; j = j + 1) begin
+        $dumpvars(0, ppsStrobe_d[j]);
+    end
+end
+
+//////////////////////////////////
+// DUT
+//////////////////////////////////
+
+reg [7:0] evCode = 0;
+reg evCodeValid = 0;
+wire [TIMESTAMP_WIDTH-1:0] timestamp;
+wire [TIMESTAMP_WIDTH-1:0] timestampHA;
+wire timestampValid;
+wire timestampHAValid;
+
+todReceiver #(
+    .NOMINAL_CLK_RATE(NOMINAL_CLK_RATE),
+    .TIMESTAMP_WIDTH(TIMESTAMP_WIDTH)
+) DUT (
+    .clk(clk),
+    .rst(1'b0),
+
+    .evCode(evCode),
+    .evCodeValid(evCodeValid),
+
+    .tooManyBitsCounter(),
+    .tooFewBitsCounter(),
+    .outOfSeqCounter(),
+    .timestamp(timestamp),
+    .timestampValid(timestampValid),
+    .timestampHA(timestampHA),
+    .timestampHAValid(timestampHAValid)
+);
+
+//////////////////////////////////
+// Stimulus
+//////////////////////////////////
+localparam TOD_DELAY = 32;
+localparam TOD_DELAY_WIDTH = $clog2(TOD_DELAY+1) + 1;
+localparam TOD_BIT_COUNTER_WIDTH = $clog2(TOD_SECONDS_WIDTH+1) + 1;
+
+reg todStart = 0;
+reg [TOD_SECONDS_WIDTH-1:0] secondsNext = 32'h12345677;
+reg [TOD_SECONDS_WIDTH-1:0] todShiftReg = 0;
+reg [TOD_DELAY_WIDTH-1:0] todDelay = TOD_DELAY - 1;
+reg [TOD_BIT_COUNTER_WIDTH-1:0] todBitCounter = TOD_SECONDS_WIDTH - 1;
+wire todDelayDone = todDelay[TOD_DELAY_WIDTH-1];
+wire todBitCounterDone = todBitCounter[TOD_BIT_COUNTER_WIDTH-1];
+
+reg ppsRequest = 0;
+reg todRequest = 0;
+
+always @(posedge clk) begin
+    if (ppsStrobe) begin
+        secondsNext <= secondsNext + 1;
+        ppsRequest <= 1;
+        todDelay <= TOD_DELAY - 1;
+        todBitCounter <= TOD_SECONDS_WIDTH - 1;
+        todStart <= 1;
+    end
+    else if (todDelayDone) begin
+        if (!todBitCounterDone && !todRequest) begin
+            todRequest <= 1;
+            todBitCounter <= todBitCounter - 1;
+
+            if (todStart) begin
+                todStart <= 0;
+                todShiftReg <= secondsNext;
+            end
+            else begin
+                todShiftReg <= {todShiftReg[TOD_SECONDS_WIDTH-2:0], 1'bx};
+            end
+        end
+    end
+    else begin
+        todDelay <= todDelay + 1;
+    end
+end
+
+always @(posedge clk) begin
+    evCode <= 0;
+    evCodeValid <= 0;
+
+    if (ppsRequest) begin
+        ppsRequest <= 0;
+
+        evCode <= EVCODE_SECONDS_MARKER;
+        evCodeValid <= 1;
+    end
+    else if (todRequest) begin
+        todRequest <= 0;
+
+        evCode <= todShiftReg[TOD_SECONDS_WIDTH-1] ? EVCODE_SHIFT_ONE :
+            EVCODE_SHIFT_ZERO;
+        evCodeValid <= 1;
+    end
+end
+
+//////////////////////////////////
+// Checks
+//////////////////////////////////
+integer timestampInvCounter = 0;
+wire [TOD_SECONDS_WIDTH-1:0] tstampSecs = timestamp[TOD_FRACTION_WIDTH+:TOD_SECONDS_WIDTH];
+wire [TOD_FRACTION_WIDTH-1:0] tstampFraction = timestamp[0+:TOD_FRACTION_WIDTH];
+
+always @(posedge clk) begin
+    // It takes 2 clock cycles after ppsStrobe for the
+    // todReceiver to perceive that + 1 clock cycle for
+    // the seconds latch
+    if (ppsStrobe_d[2]) begin
+        if (!timestampHAValid) begin
+            timestampInvCounter = timestampInvCounter + 1;
+            if (timestampInvCounter > TIMESTAMP_INVALID_ALLOWANCE) begin
+                errors = errors + 1;
+                $display("ERROR: Timestamp was not valid after %d PPS",
+                    TIMESTAMP_INVALID_ALLOWANCE);
+            end
+        end
+        else begin
+            if (secondsNext != tstampSecs + 1) begin
+                errors = errors + 1;
+                $display("ERROR: Unexpected seconds: %d, expected: %d",
+                    tstampSecs, secondsNext);
+            end
+        end
+    end
+end
+
+endmodule
