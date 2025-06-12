4d35ca4997d520435bfa1c68d2da8aea47860d9d
Alessio Amodio <aamodio@lbl.gov>
2025-06-11 18:31:19 -0700

    dsp/moving_average/moving_average.py: fixing yosys by migrating to migen

--- dsp/moving_average/moving_average.py
+++ dsp/moving_average/moving_average.py
@@ -1,58 +1,64 @@
 '''
-nMigen based
+Migen based
 '''
-from nmigen import Signal, Elaboratable, Module, Array, signed
-from nmigen.back import verilog, pysim
+from migen import Signal, Module, Array, If, ClockDomain, run_simulation
+from migen.fhdl import verilog
 from sys import stderr
 
 
-class MovingAverage(Elaboratable):
+class MovingAverage(Module):
     def __init__(self, MAX_DELAY_BITS=5, dw=16):
         self._dw = dw
         self.MAX_DELAY_BITS = MAX_DELAY_BITS
         self._SREG_LEN = 2**MAX_DELAY_BITS
-        self.i, self.o = (Signal(signed(self._dw), name='i'),
-                          Signal(signed(self._dw), name='o'))
+        self.i, self.o = (Signal(bits_sign=(self._dw, True), name='i'),
+                          Signal(bits_sign=(self._dw, True), name='o'))
         self.data_valid = Signal(name='data_valid')
+
+        # Workaround to rename default sys_clk and sys_rst to clk and rst
+        self.clk = Signal()
+        self.rst = Signal()
+        self.clock_domains.cd_sys = ClockDomain("sys")
+        self.comb += self.cd_sys.clk.eq(self.clk)
+        self.comb += self.cd_sys.rst.eq(self.rst)
+
         # log_downsample_ratio must be a power of 2.
         self.log_downsample_ratio = Signal(MAX_DELAY_BITS, name='log_downsample_ratio')
-
-    def elaborate(self, platform):
-        m = Module()
         delay_tap = Signal(self._SREG_LEN)
-        m.d.comb += delay_tap.eq(1 << self.log_downsample_ratio)
+        self.comb += delay_tap.eq(1 << self.log_downsample_ratio)
+
         # Dynamic delay shift register
-        out_val = Signal(signed(self._dw))
-        delay_reg = Array(Signal(signed(self._dw), reset=0) for _ in range(self._SREG_LEN))
+        out_val = Signal(bits_sign=(self._dw, True))
+        delay_reg = Array(Signal(bits_sign=(self._dw, True), reset=0) for _ in range(self._SREG_LEN))
         src = self.i
         for x in range(self._SREG_LEN):
-            m.d.sync += delay_reg[x].eq(src)
+            self.sync += delay_reg[x].eq(src)
             src = delay_reg[x]
-        m.d.comb += out_val.eq(delay_reg[delay_tap - 1])
-        counter = Signal(signed(self._dw))
-        with m.If(counter == delay_tap - 1):
-            m.d.sync += counter.eq(0)
-            m.d.comb += self.data_valid.eq(1)
-        with m.Else():
-            m.d.sync += counter.eq(counter + 1)
+        self.comb += out_val.eq(delay_reg[delay_tap - 1])
+        counter = Signal(bits_sign=(self._dw, True))
 
-        moving_average_full = Signal(signed(self._dw + self.MAX_DELAY_BITS))
-        m.d.sync += moving_average_full.eq(moving_average_full + self.i - out_val)
-        m.d.comb += [self.o.eq(moving_average_full >> self.log_downsample_ratio)]
-        return m
+        self.sync += [
+            If(counter == delay_tap - 1,
+               counter.eq(0)).Else(counter.eq(counter + 1))
+        ]
+
+        self.comb += [If(counter == delay_tap - 1,
+                         self.data_valid.eq(1))]
+
+        moving_average_full = Signal(bits_sign=(self._dw + self.MAX_DELAY_BITS, True))
+        self.sync += moving_average_full.eq(moving_average_full + self.i - out_val)
+        self.comb += [self.o.eq(moving_average_full >> self.log_downsample_ratio)]
 
 
 if __name__ == "__main__":
     dut = MovingAverage()
-    print(verilog.convert(dut, name='moving_average',
-                          ports=[dut.i, dut.o, dut.data_valid, dut.log_downsample_ratio]))
-
-    sim = pysim.Simulator(dut)
     log_downsample_ratio = 0
     signal_in, signal_out = [], []
-    sim.add_clock(1e-6)
+    print(verilog.convert(
+        dut, ios={dut.i, dut.o, dut.clk, dut.rst, dut.data_valid,
+                  dut.log_downsample_ratio}, name='moving_average'))
 
-    def moving_average_tb():
+    def moving_average_tb(dut):
         yield dut.log_downsample_ratio.eq(log_downsample_ratio)
         for i in range(200):
             yield dut.i.eq(i)
@@ -60,9 +66,8 @@ if __name__ == "__main__":
             signal_out.append((yield dut.o))
             yield
 
-    sim.add_sync_process(moving_average_tb)
-    with sim.write_vcd('foo.vcd', 'foo.gtkw'):
-        sim.run()
+    dut = MovingAverage()
+    run_simulation(dut, moving_average_tb(dut), clocks={"sys": 100}, vcd_name="basic1.vcd")
     print("Attempting plot; ignore warnings if running batch", file=stderr)
     try:
         from matplotlib import pyplot as plt
