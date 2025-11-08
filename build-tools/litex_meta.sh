5ef11623367b23bfe686ef30830b719517b770da
Alessio Amodio <aamodio@lbl.gov>
2025-11-07 16:03:19 -0800

    build-tools/litex_meta.sh: install litex with tag 2025.08

--- build-tools/litex_meta.sh
+++ build-tools/litex_meta.sh
@@ -3,29 +3,29 @@
 # Definitely needs network access
 # Expect to start this in a blank directory, where you have write access
 # Probably use a python venv
-# Total disk space consumed: about 474 MB
+# Total disk space consumed: about 658 MB
 # How to use litex_setup_freeze_repos()?
 set -e
 
 rm -f litex_setup.py
-# Commit a2c2c211c5 is tag: 2023.08, dated 2023-09-14
+# Commit 05ddccb206 is tag: 2025.08, dated 2025-10-03
 # See https://github.com/enjoy-digital/litex
-wget https://raw.githubusercontent.com/enjoy-digital/litex/a2c2c211c56c5d7a6a57/litex_setup.py
-echo "54597b452644db21ecea9081ce9e191a7bd3b5d38610f5e7ccee60cb17d9150d  litex_setup.py" | sha256sum -c
+wget https://raw.githubusercontent.com/enjoy-digital/litex/05ddccb206ffd02e0efc/litex_setup.py
+echo "ac835dfa7631357de28326e0c8b8dec46d1fbdab46e59679a2e6b1e709e90938  litex_setup.py" | sha256sum -c
 # patch two lines, to keep picorv32 in "standard" config, and disable auto-update
 patch litex_setup.py << EOT
-149c149
+157c157
 < standard_repos.remove("pythondata-cpu-picorv32")
 ---
 > # standard_repos.remove("pythondata-cpu-picorv32")
-447c447
+526c526
 <     if not args.dev:
 ---
 >     if False and not args.dev:
 EOT
-echo "d77081080f0c5109adc92d2730145228cb19633de7f2ff50ca3a4ec0cb341532  litex_setup.py" | sha256sum -c
+echo "d859db819bc3dba4a7120be7e2c4b60fea107fa22b54aebb5678ccc8c6a13666  litex_setup.py" | sha256sum -c
 
 # Now that we're quite sure we have the litex_setup we want,
 # go ahead and run it.
-python3 litex_setup.py --init --update --tag 2023.08 --install --config standard
+python3 litex_setup.py --init --update --tag 2025.08 --install --config standard
 echo "DONE"
