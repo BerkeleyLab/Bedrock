ec28e74c7571a8c3a28767031069526bdde3e522
sdmurthy <sdmurthy@lbl.gov>
2025-11-14 14:06:47 -0800

    comms_top: Pass CI_JOB_TOKEN to git clone qf2-pre-users repo and pass correct command for loading bitfile

--- projects/comms_top/qf2_070_3_setup.sh
+++ projects/comms_top/qf2_070_3_setup.sh
@@ -1,2 +1,2 @@
 set -e
-git clone --depth 1 --branch trixie_updates https://gitlab.lbl.gov/hdl-libraries/qf2-pre-users.git qf2-pre-users
+git clone --branch trixie_updates https://gitlab-ci-token:$CI_JOB_TOKEN@gitlab.lbl.gov/hdl-libraries/qf2-pre-users.git qf2-pre-users
