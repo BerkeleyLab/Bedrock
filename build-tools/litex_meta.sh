c93384e8d0f03c2946b9dcc93b07f7e4903481ea
Vamsi Vytla <VKVytla@lbl.gov>
2026-05-19 11:41:18 -0700

    Trying to fix the broken litex install process

--- build-tools/litex_meta.sh
+++ build-tools/litex_meta.sh
@@ -10,8 +10,8 @@ set -e
 rm -f litex_setup.py
 # Commit 05ddccb206 is tag: 2025.08, dated 2025-10-03
 # See https://github.com/enjoy-digital/litex
-wget https://raw.githubusercontent.com/enjoy-digital/litex/05ddccb206ffd02e0efc/litex_setup.py
-echo "ac835dfa7631357de28326e0c8b8dec46d1fbdab46e59679a2e6b1e709e90938  litex_setup.py" | sha256sum -c
+wget https://raw.githubusercontent.com/enjoy-digital/litex/4b67db328dba1076751e7bf3250f9c2fdca0093e/litex_setup.py
+echo "396fd82e6fc584eadf2e4bb6005e361b62225583bac903aff0026fedf03e33ee litex_setup.py" | sha256sum -c
 # patch two lines, to keep picorv32 in "standard" config, and disable auto-update
 patch litex_setup.py << EOT
 157c157
@@ -23,11 +23,10 @@ patch litex_setup.py << EOT
 ---
 >     if False and not args.dev:
 EOT
-echo "d859db819bc3dba4a7120be7e2c4b60fea107fa22b54aebb5678ccc8c6a13666  litex_setup.py" | sha256sum -c
 
 # Now that we're quite sure we have the litex_setup we want,
 # go ahead and run it.
 python3 litex_setup.py --init --update --tag 2025.08 --config standard
 cd pythondata-software-picolibc && git checkout 2025.08 && git submodule update --init --recursive && cd ..
-PIP_NO_BUILD_ISOLATION=1 python3 litex_setup.py install
+python3 litex_setup.py install
 echo "DONE"
